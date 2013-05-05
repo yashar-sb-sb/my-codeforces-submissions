1d3124e36d6e653186416058a7346e8b3e893301
Yashar Shahi <yashar_sb_sb@yahoo.com>
2013-05-05 22:32:46 +0430

    submit 3682362 for 302/C/[Yaroslav and Sequence] with lang: MS C++ & verdict: WRONG_ANSWER

---
+++ 300-399/302/(3682362)[WRONG_ANSWER]C[ b'Yaroslav and Sequence' ].cpp
@@ -0,0 +1,85 @@
+#include<iostream>
+#include<conio.h>
+#include<vector>
+#include<cmath>
+#include<algorithm>
+using namespace std;
+int main()
+{
+    int n,plus=0,minus=0,zero=0,sum=0;
+    cin>>n;
+    vector<int> vect(2*n-1);
+    for (int i = 0; i < 2*n-1; i++)
+    {
+        cin>>vect[i];
+        if (vect[i]>0)
+        {
+            plus++;
+        }
+        else if (vect[i]<0)
+        {
+            minus++;
+        }
+        else
+        {
+            zero++;
+        }
+    }
+    for (int i = 0; i < 2*n-1; i++)
+    {
+        for (int j = i; j > 0; j--)
+        {
+            if (abs(vect[j])<abs(vect[j-1]))
+            {
+                swap(vect[j],vect[j-1]);
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+    if (!(n%2))
+    {
+        sum+=-abs(vect[0]);
+        for (int i = 1; i < 2*n-1; i++)
+        {
+            sum+=abs(vect[i]);
+        }
+    }
+    else
+    {
+        for (int i = 0; i < 2*n-1; i++)
+        {
+            sum+=abs(vect[i]);
+        }
+    }
+    int res = sum;
+    sum=0;
+    if (minus>=n)
+    {
+        minus-=n;
+        for (int i = 0; i < 2*n-1-minus; i++)
+        {
+            sum+=abs(vect[2*n-2-i]);
+        }
+        for (int i = 2*n-1-minus; i < 2*n-1; i++)
+        {
+            sum+=-abs(vect[2*n-2-i]);
+        }
+    }
+    else
+    {
+        minus = min(minus,n-minus);
+        for (int i = 0; i < 2*n-1-minus; i++)
+        {
+            sum+=abs(vect[2*n-2-i]);
+        }
+        for (int i = 2*n-1-minus; i < 2*n-1; i++)
+        {
+            sum+=-abs(vect[2*n-2-i]);
+        }
+    }
+    res = max(sum,res);
+    cout<<res;
+}
