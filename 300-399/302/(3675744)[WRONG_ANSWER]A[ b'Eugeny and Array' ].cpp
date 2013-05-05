027712233ca32cccc1a7fbfdba38949f211b6066
Yashar Shahi <yashar_sb_sb@yahoo.com>
2013-05-05 20:57:23 +0430

    submit 3675744 for 302/A/[Eugeny and Array] with lang: MS C++ & verdict: WRONG_ANSWER

---
+++ 300-399/302/(3675744)[WRONG_ANSWER]A[ b'Eugeny and Array' ].cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<conio.h>
+#include<vector>
+#include<cmath>
+using namespace std;
+int main()
+{
+    int n,m,inp,inp1,inp2;
+    cin>>n>>m;
+    int minOne=0,plusOne=0;
+    for (int i = 0; i < n; i++)
+    {
+        cin>>inp;
+        if (inp>0)
+        {
+            plusOne++;
+        }
+        else if (inp<0)
+        {
+            minOne++;
+        }
+    }
+    for (int i = 0; i < m; i++)
+    {
+        cin>>inp1>>inp2;
+        if ((inp1-inp2)%2&&inp1-inp2<=min(plusOne,minOne))
+        {
+            cout<<1<<endl;
+        }
+        else
+        {
+            cout<<0<<endl;
+        }
+    }
+}
