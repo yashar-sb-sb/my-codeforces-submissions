ff615ea980063eb5f0cabb12f23749b9e59b93f4
Yashar Shahi <yashar_sb_sb@yahoo.com>
2013-05-19 19:08:55 +0430

    submit 3744069 for 305/B/[Continued Fractions] with lang: MS C++ & verdict: WRONG_ANSWER

---
+++ 300-399/305/(3744069)[WRONG_ANSWER]B[ b'Continued Fractions' ].cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<conio.h>
+#include<cmath>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+unsigned long long gcd(unsigned long long a,unsigned long long b)
+{
+    if (!b)
+    {
+        return a;
+    }
+    return gcd(b,a%b);
+}
+
+void main()
+{
+    int n;
+    unsigned long long p,q,a=0,b=1,g;
+    cin>>p>>q>>n;
+    vector<unsigned long long>A(n);
+    g = p>q ? gcd(p,q) : gcd(q,p);
+    p/=g;
+    q/=g;
+    for (int i = 0; i < n; i++)
+    {
+        cin>>A[i];
+    }
+    for (int i = n; i; --i)
+    {
+        a+=A[i-1]*b;
+        g = a>b ? gcd(a,b) : gcd(b,a);
+        a/=g;
+        b/=g;
+        swap(a,b);
+    }
+    if (b==p&&a==q)
+    {
+        cout<<"YES";
+    }
+    else
+    {
+        cout<<"NO";
+    }
+}
