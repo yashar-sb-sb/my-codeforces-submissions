664bff91777aaea3ba81e8a6cb19b2486f32386a
Yashar Shahi <yashar_sb_sb@yahoo.com>
2013-06-20 19:56:36 +0430

    submit 3929241 for 317/A/[Perfect Pair] with lang: MS C++ & verdict: WRONG_ANSWER

---
+++ 300-399/317/(3929241)[WRONG_ANSWER]A[ b'Perfect Pair' ].cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x,y,m,count,t;
+    cin>>x>>y>>m;
+    if (x<y)
+    {
+        count = x;
+        x = y;
+        y = count;
+    }
+    count = -1;
+    if (x>=m||y>=m||(x+y)-m>min(x,y)-m)
+    {
+        count++;
+        while (x<m&&y<m)
+        {
+            count++;
+            t = x;
+            x = x + y;
+            y = t;
+        }
+    }
+    cout<<count;
+}
