fa0d82b244987c2eba7d99ac5333bcf54d324784
Yashar Shahi <yashar_sb_sb@yahoo.com>
2013-06-20 20:04:28 +0430

    submit 3929262 for 317/A/[Perfect Pair] with lang: MS C++ & verdict: TIME_LIMIT_EXCEEDED

---
+++ 300-399/317/(3929262)[TIME_LIMIT_EXCEEDED]A[ b'Perfect Pair' ].cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    long long x,y,m,count,t;
+    cin>>x>>y>>m;
+    if (x<y)
+    {
+        count = x;
+        x = y;
+        y = count;
+    }
+    count = -1;
+    if (x>=m||y>=m||(x+y)-m>min(x,y)-m)
+    {
+        count++;
+        while (x<m&&y<m)
+        {
+            count++;
+            t = x;
+            x = x + y;
+            y = t;
+            if (x<y)
+            {
+                t = x;
+                x = y;
+                y = t;
+            }
+        }
+    }
+    cout<<count;
+}
