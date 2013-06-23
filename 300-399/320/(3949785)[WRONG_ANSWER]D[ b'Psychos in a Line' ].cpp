eb6a34c46f149c624e60e96b10dc7d886187ccd9
Yashar Shahi <yashar_sb_sb@yahoo.com>
2013-06-23 19:19:04 +0430

    submit 3949785 for 320/D/[Psychos in a Line] with lang: MS C++ & verdict: WRONG_ANSWER

---
+++ 300-399/320/(3949785)[WRONG_ANSWER]D[ b'Psychos in a Line' ].cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<conio.h>
+#include<cmath>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main()
+{
+    int n,pst,now,count=0,mas,cp,mcp,ocp;
+    cin>>n;
+    vector<int> v(n);
+    now = n;
+    for (int i = 0; i < n; i++)
+    {
+        cin>>v[i];
+    }
+    do
+    {
+        cp=0;
+        pst = now;
+        n=0;
+        mas = 0;
+        mcp=0;
+        ocp=0;
+        for (int i = 1, j = 1; i < now; i++)
+        {
+            if (v[mas]>v[i]&&v[i]>v[i-1])
+            {
+                v[j]=v[i];
+                n++;
+                mcp++;
+                if (cp<mcp)
+                {
+                    cp = mcp;
+                }
+            }
+            else
+            {
+                mas = i - 1;
+                mcp=1;
+                if (v[i-1]>v[i])
+                {
+                    ocp=1;
+                    v[j] = v[i];
+                    n++;
+                }
+                else
+                {
+                    v[j++] = v[i];
+                }
+            }
+        }
+        now-=n;
+        count+=cp;
+    } while (pst>now);
+    cout<<count;
+    return 0;
+}
