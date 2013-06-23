8f8d03b37dbd278c72af13650eb3d09758cf4a22
Yashar Shahi <yashar_sb_sb@yahoo.com>
2013-06-23 17:48:48 +0430

    submit 3944185 for 320/A/[Magic Numbers] with lang: MS C++ & verdict: WRONG_ANSWER

---
+++ 300-399/320/(3944185)[WRONG_ANSWER]A[ b'Magic Numbers' ].cpp
@@ -0,0 +1,110 @@
+#include<iostream>
+#include<conio.h>
+#include<cmath>
+#include<vector>
+#include<algorithm>
+#include<string>
+using namespace std;
+int main()
+{
+    string str;
+    cin>>str;
+    int i;
+    if ((i = str.find("144")) != -1)
+    {
+        str.replace(i,3,"");
+    }
+    if ((i = str.find("144")) != -1)
+    {
+        str.replace(i,3,"");
+    }
+    if ((i = str.find("144")) != -1)
+    {
+        str.replace(i,3,"");
+    }
+    if ((i = str.find("14")) != -1)
+    {
+        str.replace(i,2,"");
+    }
+    if ((i = str.find("14")) != -1)
+    {
+        str.replace(i,2,"");
+    }
+    if ((i = str.find("14")) != -1)
+    {
+        str.replace(i,2,"");
+    }
+    if ((i = str.find("14")) != -1)
+    {
+        str.replace(i,2,"");
+    }
+    if ((i = str.find("14")) != -1)
+    {
+        str.replace(i,2,"");
+    }
+    if ((i = str.find("14")) != -1)
+    {
+        str.replace(i,2,"");
+    }
+    if ((i = str.find("14")) != -1)
+    {
+        str.replace(i,2,"");
+    }
+    if ((i = str.find("111")) != -1)
+    {
+        str.replace(i,3,"");
+    }
+    if ((i = str.find("11")) != -1)
+    {
+        str.replace(i,2,"");
+    }
+    if ((i = str.find("11")) != -1)
+    {
+        str.replace(i,2,"");
+    }
+    if ((i = str.find("1")) != -1)
+    {
+        str.replace(i,1,"");
+    }
+    if ((i = str.find("1")) != -1)
+    {
+        str.replace(i,1,"");
+    }
+    if ((i = str.find("1")) != -1)
+    {
+        str.replace(i,1,"");
+    }
+    if ((i = str.find("1")) != -1)
+    {
+        str.replace(i,1,"");
+    }
+    if ((i = str.find("1")) != -1)
+    {
+        str.replace(i,1,"");
+    }
+    if ((i = str.find("1")) != -1)
+    {
+        str.replace(i,1,"");
+    }
+    if ((i = str.find("1")) != -1)
+    {
+        str.replace(i,1,"");
+    }
+    if ((i = str.find("1")) != -1)
+    {
+        str.replace(i,1,"");
+    }
+    if ((i = str.find("1")) != -1)
+    {
+        str.replace(i,1,"");
+    }
+    if (!str.size())
+    {
+        cout<<"YES";
+    }
+    else
+    {
+        cout<<"NO";
+    }
+    return 0;
+}
