44bb11169d17b2a9051e297f01b1c2fa406b95f2
Yashar Shahi <yashar_sb_sb@yahoo.com>
2013-06-27 21:39:55 +0430

    submit 3968036 for 4/A/[Watermelon] with lang: MS C++ & verdict: OK

---
+++ 0-99/4/(3968036)[OK]A[ b'Watermelon' ].cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int w;
+    cin>> w;
+    if (!(w%2)&&w>3)
+    {
+        cout<<"YES";
+    }
+    else
+    {
+        cout<<"NO";
+    }
+}
