03c5b80edd85e1e744a193c5d58bacac71f7be19
Yashar Shahi <yashar_sb_sb@yahoo.com>
2013-07-03 18:52:15 +0430

    submit 4006957 for 268/A/[Games] with lang: MS C++ & verdict: WRONG_ANSWER

---
+++ 200-299/268/(4006957)[WRONG_ANSWER]A[ b'Games' ].cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int count=0,n,h,a,A[30][2];
+    cin>>n;
+    for (int i = 0; i < n; i++)
+    {
+        cin>>A[i][0]>>A[i][1];
+    }
+    int l = n-1;
+    for (int i = 0; i < l; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (A[i][0] == A[j][1] || A[i][1] == A[j][0])
+            {
+                ++count;
+            }
+        }
+    }
+    cout << count;
+}
