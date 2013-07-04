5da5aed83fe9794521e0d2b2d3d1a1b57cec11c6
Yashar Shahi <yashar_sb_sb@yahoo.com>
2013-07-04 23:07:17 +0430

    submit 4023021 for 61/A/[Ultra-Fast Mathematician] with lang: MS C++ & verdict: OK

---
+++ 0-99/61/(4023021)[OK]A[ b'Ultra-Fast Mathematician' ].cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string s1,s2;
+    cin>>s1>>s2;
+    int l = s1.size();
+    for (int i = 0; i < l; i++)
+    {
+        if (s1[i]==s2[i])
+        {
+            cout<<0;
+        }
+        else
+        {
+            cout<<1;
+        }
+    }
+}
