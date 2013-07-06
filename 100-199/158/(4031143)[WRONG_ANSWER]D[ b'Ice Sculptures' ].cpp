3689d9ffc8902a16f7776795eaa6bea3cdb78978
Yashar Shahi <yashar_sb_sb@yahoo.com>
2013-07-06 18:10:13 +0430

    submit 4031143 for 158/D/[Ice Sculptures] with lang: GNU C++ & verdict: WRONG_ANSWER

---
+++ 100-199/158/(4031143)[WRONG_ANSWER]D[ b'Ice Sculptures' ].cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+using namespace std;
+int main()
+{
+    int n,nn,s=0,ms=-3001;
+    cin>>n;
+    vector<int> v(n);//6 -> 1 -2 3 -4 5 -6
+	for(int i = 0; i < n; ++i)
+	{
+		cin>>v[i];
+	}
+    int l = n/3;
+    for (int i = 1; i <= l; i++)
+    {
+		for(int j = 0; j < i; j++)
+		{
+			s=0;
+			for (int ii = j; ii < n; ii+=i)
+			{
+				s+=v[ii];
+			}
+			if (s>ms)
+			{
+				ms = s;
+			}
+		}
+    }
+    cout<<ms;
+}
