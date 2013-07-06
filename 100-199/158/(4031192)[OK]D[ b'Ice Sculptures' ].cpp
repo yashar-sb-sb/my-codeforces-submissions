0d8fecd79c59a40f7fdeec1c2e62c2505bc05a45
Yashar Shahi <yashar_sb_sb@yahoo.com>
2013-07-06 18:22:40 +0430

    submit 4031192 for 158/D/[Ice Sculptures] with lang: GNU C++ & verdict: OK

---
+++ 100-199/158/(4031192)[OK]D[ b'Ice Sculptures' ].cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+using namespace std;
+int main()
+{
+    long long n,nn,s=0,ms=0;
+    cin>>n;
+    vector<int> v(n);//6 -> 1 -2 3 -4 5 -6
+	for(int i = 0; i < n; ++i)
+	{
+		cin>>v[i];
+		ms+=v[i];
+	}
+    int l = n/3;
+    for (int i = 1; i <= l; i++)
+    {
+		if(!(n%i))
+		{
+			for(int j = 0; j < i; j++)
+			{
+				s=0;
+				for (int ii = j; ii < n; ii+=i)
+				{
+					s+=v[ii];
+				}
+				if (s>ms)
+				{
+					ms = s;
+				}
+			}
+		}
+    }
+    cout<<ms;
+}
