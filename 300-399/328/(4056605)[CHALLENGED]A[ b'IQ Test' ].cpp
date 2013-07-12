ac34a3efa79666efe48e8236cc6674831108c6a9
Yashar Shahi <yashar_sb_sb@yahoo.com>
2013-07-13 02:12:47 +0430

    submit 4056605 for 328/A/[IQ Test] with lang: GNU C++ & verdict: CHALLENGED

---
+++ 300-399/328/(4056605)[CHALLENGED]A[ b'IQ Test' ].cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+
+using namespace std;
+
+typedef long long LL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+	int *A = new int[4];
+	for(int i = 0; i < 4; ++i)cin>>A[i];
+	if(A[0]-A[1] == A[1]-A[2] && A[0]-A[1] == A[2]-A[3])
+	{
+		cout<<A[3]+A[3]-A[2];
+	}
+	else if(A[0]!=0&&A[1]!=0&&A[2]!=0&&A[3]!=0&&A[0]!=A[1]&&A[1]/A[0] == A[2]/A[1] && A[1]/A[0] == A[3]/A[2]&& !(A[1]%A[0]) && !(A[2]%A[1]) && !(A[3]/A[2])&&!(A[3]*A[3]%A[2]))
+	{
+		cout<<A[3]*A[3]/A[2];
+	}
+	else
+	{
+		cout<<42;
+	}
+	return 0;
+}
