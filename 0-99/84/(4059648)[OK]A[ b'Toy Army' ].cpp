4e37efe30def22761d6b93ce7968d8adefd02f94
Yashar Shahi <yashar_sb_sb@yahoo.com>
2013-07-13 17:12:43 +0430

    submit 4059648 for 84/A/[Toy Army] with lang: GNU C++ & verdict: OK

---
+++ 0-99/84/(4059648)[OK]A[ b'Toy Army' ].cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+
+using namespace std;
+
+typedef long long LL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+	int n;
+	cin>> n;
+	cout<<n+n/2;
+	return 0;
+}
