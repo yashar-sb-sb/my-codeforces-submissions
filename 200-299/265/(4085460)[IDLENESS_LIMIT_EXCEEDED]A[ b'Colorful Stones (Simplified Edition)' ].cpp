40b3a93791587cae5c4b37aafa604c6df14a7777
Yashar Shahi <yashar_sb_sb@yahoo.com>
2013-07-17 13:01:25 +0430

    submit 4085460 for 265/A/[Colorful Stones (Simplified Edition)] with lang: GNU C++ & verdict: IDLENESS_LIMIT_EXCEEDED

---
+++ 200-299/265/(4085460)[IDLENESS_LIMIT_EXCEEDED]A[ b'Colorful Stones (Simplified Edition)' ].cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<conio.h>
+
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+using namespace std;
+
+typedef long long LL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+char s[51],c;
+int i=0;
+int main()
+{
+	cin>>s;
+	c=getch();
+	while(c>'A'&&c<'S')
+	{
+		if(s[i]==c)++i;
+		c=getch();
+	}
+	cout<<++i;
+	return 0;
+}
