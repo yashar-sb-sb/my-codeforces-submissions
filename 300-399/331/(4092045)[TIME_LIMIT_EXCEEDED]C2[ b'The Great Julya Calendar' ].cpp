0e5c62ee76bda7c153ff8716e82adbc4d3c9a9da
Yashar Shahi <yashar_sb_sb@yahoo.com>
2013-07-17 21:36:12 +0430

    submit 4092045 for 331/C2/[The Great Julya Calendar] with lang: GNU C++ & verdict: TIME_LIMIT_EXCEEDED

---
+++ 300-399/331/(4092045)[TIME_LIMIT_EXCEEDED]C2[ b'The Great Julya Calendar' ].cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+using namespace std;
+
+typedef long long LL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+	long long n,i=0,t,m;
+	cin>>n;
+	while(n)
+	{
+		++i;
+		m=n%10;
+		t=n/10;
+		while(t)
+		{
+			if(t%10>m)m=t%10;
+			t/=10;
+		}
+		n-=m;
+	}
+	cout<<i;
+	return 0;
+}
