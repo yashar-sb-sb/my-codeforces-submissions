338921df535fcaa81e5a5b6dadb6c10a75378866
Yashar Shahi <yashar_sb_sb@yahoo.com>
2013-07-20 11:42:57 +0430

    submit 4110205 for 221/A/[Little Elephant and Function] with lang: GNU C++ & verdict: OK

---
+++ 200-299/221/(4110205)[OK]A[ b'Little Elephant and Function' ].cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+using namespace std;
+
+typedef long long LL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+	int n;
+	cin>>n;
+	cout<<n;
+	for(int i = 1; i < n; ++i)cout<<' '<<i;
+	return 0;
+}
