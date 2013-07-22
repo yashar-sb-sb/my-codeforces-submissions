039bd503a6a8b4f02c28419ba833f80db308082b
Yashar Shahi <yashar_sb_sb@yahoo.com>
2013-07-22 16:53:20 +0430

    submit 4135391 for 114/A/[Cifera] with lang: GNU C++ & verdict: TIME_LIMIT_EXCEEDED

---
+++ 100-199/114/(4135391)[TIME_LIMIT_EXCEEDED]A[ b'Cifera' ].cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+ios_base::sync_with_stdio(0);cin.tie(0);
+	int k,l,c=0,t;
+	cin>>k>>l;
+	t = k;
+	while(t<l)
+	{
+		t*=k;
+		++c;
+	}
+	if(t==l)
+	{
+		cout << "YES\n"<<c;
+	}
+	else
+	{
+		cout<< "NO";
+	}
+	return 0;
+}
