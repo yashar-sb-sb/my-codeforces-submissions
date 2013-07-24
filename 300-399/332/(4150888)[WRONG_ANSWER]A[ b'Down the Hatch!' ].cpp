701a9d59275bd4684e906f0801ed1af78a9f0ad5
Yashar Shahi <yashar_sb_sb@yahoo.com>
2013-07-24 20:49:25 +0430

    submit 4150888 for 332/A/[Down the Hatch!] with lang: GNU C++ & verdict: WRONG_ANSWER

---
+++ 300-399/332/(4150888)[WRONG_ANSWER]A[ b'Down the Hatch!' ].cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+//ios_base::sync_with_stdio(0);
+	int n,i=0,j=0,co=0;
+	cin>>n;
+	char c,t='e';
+	//cin>>t>>c;
+	//t = c;
+	do
+	{
+		if(t==c)++i;
+		else i = 0;
+		if(!(j%n)&& i > 2)++co;
+		else t = c;
+		++j;
+	}while(cin>>c);
+	cout<<co;
+	return 0;
+}
