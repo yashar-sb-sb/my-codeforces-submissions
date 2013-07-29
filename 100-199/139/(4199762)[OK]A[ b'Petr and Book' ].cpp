b0e045a5404e1805374a7be2ad49da82cd9f0f2f
Yashar Shahi <yashar_sb_sb@yahoo.com>
2013-07-29 18:00:23 +0430

    submit 4199762 for 139/A/[Petr and Book] with lang: GNU C++ & verdict: OK

---
+++ 100-199/139/(4199762)[OK]A[ b'Petr and Book' ].cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	int n,A[7],s=0,i;
+	cin>>n;
+	for(i=0; i < 7; ++i)
+	{
+		cin>>A[i];
+		s+=A[i];
+	}
+	n%=s;
+	if(!n)n=s;
+	for(i = 0; n>0; n-=A[i++]);
+	cout<<(i-1)%7+1;
+	return 0;
+}
