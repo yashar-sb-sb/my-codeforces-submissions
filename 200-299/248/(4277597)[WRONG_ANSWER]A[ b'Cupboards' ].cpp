8847b22ec9a08b405e62e512076a091d4d260b6a
Yashar Shahi <yashar_sb_sb@yahoo.com>
2013-08-14 16:34:29 +0430

    submit 4277597 for 248/A/[Cupboards] with lang: GNU C++ & verdict: WRONG_ANSWER

---
+++ 200-299/248/(4277597)[WRONG_ANSWER]A[ b'Cupboards' ].cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	int n,l,r,t1=0,t2=0;
+	cin>>n;
+	while(n--)
+	{
+		cin>>l>>r;
+		if(l)++t1;
+		else ++t2;
+		if(r)++t2;
+		else ++t1;
+	}
+	cout<<min(t1,t2);
+	return 0;
+}
