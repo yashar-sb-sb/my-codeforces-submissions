45d5891348029fa79cc7e650391e8a9345a4c4c6
Yashar Shahi <yashar_sb_sb@yahoo.com>
2013-08-29 20:48:07 +0430

    submit 4366872 for 339/B/[Xenia and Ringroad] with lang: GNU C++ & verdict: OK

---
+++ 300-399/339/(4366872)[OK]B[ b'Xenia and Ringroad' ].cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	int n,m,c=1;
+	LL s=0;
+	cin>>n>>m;
+	while(cin>>m)
+	{
+		if(c<m)s+=m-c;
+		else if(c>m)s+=n-c+m;
+		c = m;
+	}
+	cout <<s;
+	return 0;
+}
