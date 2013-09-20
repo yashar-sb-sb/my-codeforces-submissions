c444bbd4385ef66c9e28d78f2e6f4d5c9a0ca31a
Yashar Shahi <yashar_sb_sb@yahoo.com>
2013-09-20 20:16:58 +0430

    submit 4512961 for 347/B/[Fixed Points] with lang: GNU C++ & verdict: WRONG_ANSWER

---
+++ 300-399/347/(4512961)[WRONG_ANSWER]B[ b'Fixed Points' ].cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	int n,A[100000],c=0,flag=0;
+	cin>>n;
+	for(int i = 0; i < n; ++i)cin>>i;
+	for(int i = 0; i < n; ++i)
+	{
+		if(A[i] == i)++c;
+		else if(A[i] == A[A[i]])++flag;
+	}
+	if(flag)c+=2;
+	else ++c;
+	if(c>n)c=n;
+	cout<<c;
+	return 0;
+}
