2d29968b2bb70cbf1e6f28eed1b07fb5caf501e7
Yashar Shahi <yashar_sb_sb@yahoo.com>
2013-09-21 11:45:09 +0430

    submit 4527992 for 205/A/[Little Elephant and Rozdil] with lang: GNU C++ & verdict: OK

---
+++ 200-299/205/(4527992)[OK]A[ b'Little Elephant and Rozdil' ].cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	int n,m,c=0,inp,j=1;
+	cin>>n;
+	cin>>m;
+	for(int i = 1; i < n; ++i)
+	{
+		cin>>inp;
+		if(inp<m)m = inp,c=0,j = i + 1;
+		else if(inp == m)++c;
+	}
+	if(c)cout<<"Still Rozdil";
+	else cout<<j;
+	return 0;
+}
