a68e20fdc2bd940b7617f029371c2e882cc8d361
Yashar Shahi <yashar_sb_sb@yahoo.com>
2013-10-03 18:06:58 +0330

    submit 4649932 for 230/B/[T-primes] with lang: GNU C++ & verdict: WRONG_ANSWER

---
+++ 200-299/230/(4649932)[WRONG_ANSWER]B[ b'T-primes' ].cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	uLL inp,i=0,c=0,lim;
+	int n;
+	cin>>n;
+	while(n--)
+	{
+		cin>>inp;
+		lim = sqrt(inp)+1;
+		if(lim>=inp)lim = inp-1;
+		while(i++ < lim)
+		{
+			if(!(inp%i))++c;
+		}
+		if(c == 2)cout<<"YES\n";
+		else cout<<"NO\n";
+	}
+	return 0;
+}
