30a6ba1069535d9469b340716e2c11ab7e168a93
Yashar Shahi <yashar_sb_sb@yahoo.com>
2013-10-04 19:07:52 +0330

    submit 4658529 for 352/A/[Jeff and Digits] with lang: GNU C++ & verdict: WRONG_ANSWER

---
+++ 300-399/352/(4658529)[WRONG_ANSWER]A[ b'Jeff and Digits' ].cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+#include<bitset>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	int n,f=0,z=0,inp;
+	cin>>n;
+	for(int i = 0; i < n; ++i)
+	{
+		cin>>inp;
+		if(inp)++f;
+		else ++z;
+	}
+	f = f/9*9;
+	if(f&&z&&!(f%9))
+	{
+		while(f--)cout<<5;
+		while(z--)cout<<0;
+	}
+	else cout<<0;
+	return 0;
+}
