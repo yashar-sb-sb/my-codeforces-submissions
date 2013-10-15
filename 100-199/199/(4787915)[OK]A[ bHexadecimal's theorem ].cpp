3a85d62ac3db3482347c4584faa425f7bfb6bdf2
Yashar Shahi <yashar_sb_sb@yahoo.com>
2013-10-15 10:59:41 +0330

    submit 4787915 for 199/A/[Hexadecimal's theorem] with lang: GNU C++ & verdict: OK

---
+++ 100-199/199/(4787915)[OK]A[ bHexadecimal's theorem ].cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+#include<bitset>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	int n;
+	cin>>n;
+	cout<<"0 0 "<<n;
+	return 0;
+}
