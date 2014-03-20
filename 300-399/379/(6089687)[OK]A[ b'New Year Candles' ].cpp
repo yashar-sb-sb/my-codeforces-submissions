2f5ebb8139d4df4cbabd05300c44bd7390d77753
Yashar Shahi <yashar_sb_sb@yahoo.com>
2014-03-20 13:19:29 +0330

    submit 6089687 for 379/A/[New Year Candles] with lang: GNU C++ & verdict: OK

---
+++ 300-399/379/(6089687)[OK]A[ b'New Year Candles' ].cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+#include<bitset>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	int a,b;
+	cin>>a>>b;
+	cout<<a/(b-1)*b+(a%(b-1)==0?-1:a%(b-1));
+	return 0;
+}
