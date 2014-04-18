b4bb80c05adc04188d0885bb29cd2b0fbfecbf89
Yashar Shahi <yashar_sb_sb@yahoo.com>
2014-04-18 22:19:12 +0430

    submit 6410522 for 412/E/[E-mail Addresses] with lang: GNU C++ & verdict: WRONG_ANSWER

---
+++ 400-499/412/(6410522)[WRONG_ANSWER]E[ b'E-mail Addresses' ].cpp
@@ -0,0 +1,83 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+#include<bitset>
+#include<string>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	string s;
+	int n,l=0,r=0,a1=-1,a2=-1,a3=-1;
+	long long sum = 0;
+	cin>>s;
+	n = s.size();
+	for(int i = 0; i < n; ++i)
+	{
+		if(s[i] == '@')
+		{
+			a1 = a2;
+			a2 = a3;
+			a3 = i;
+			if(a1 != a2)
+			{
+				l = 0;
+				int j;
+				for(j = a1 + 1; j < a2; ++j)
+				{
+					if(islower(s[j]))++l;
+					else if(s[j] == '.')l=0;
+				}
+				r = 0;
+				for(j = a2 + 1; j < a3&&s[j]!='.'&&(islower(s[j])||isdigit(s[j])); ++j);
+				if(s[j]=='.')
+				{
+					for(++j; j < a3; ++j)
+					{
+						if(islower(s[j]))++r;
+						else break;
+					}
+				}
+				sum += l * r;
+			}
+		}
+	}
+	
+			a1 = a2;
+			a2 = a3;
+			a3 = n;
+				l = 0;
+				int j;
+				for(j = a1 + 1; j < a2; ++j)
+				{
+					if(islower(s[j]))++l;
+					else if(s[j] == '.')l=0;
+				}
+				r = 0;
+				for(j = a2 + 1; j < a3&&s[j]!='.'&&(islower(s[j])||isdigit(s[j])); ++j);
+				if(s[j]=='.')
+				{
+					for(++j; j < a3; ++j)
+					{
+						if(islower(s[j]))++r;
+						else break;
+					}
+				}
+				sum += l * r;
+	cout<<sum;
+	return 0;
+}
