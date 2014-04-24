61821139c2eab25898288b58ae0f776fc648afd7
Yashar Shahi <yashar_sb_sb@yahoo.com>
2014-04-24 19:32:02 +0430

    submit 6461056 for 276/B/[Little Girl and Game] with lang: GNU C++ & verdict: WRONG_ANSWER

---
+++ 200-299/276/(6461056)[WRONG_ANSWER]B[ b'Little Girl and Game' ].cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+#include<bitset>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	int A[26],s=0,i=26, t=0;
+	char c;
+	fill(A,A+26,0);
+	while(cin>>c)++A[c-'a'],++t;
+	while(i--)
+	{
+		if(i%2)++s;
+	}
+	cout<<(s<2||s%2?"First":"Second");
+	return 0;
+}
