43a806cb937073839d0d27cdd5a99fb80ce34e1d
Yashar Shahi <yashar_sb_sb@yahoo.com>
2014-04-25 10:10:47 +0430

    submit 6463619 for 355/A/[Vasya and Digital Root] with lang: GNU C++ & verdict: WRONG_ANSWER

---
+++ 300-399/355/(6463619)[WRONG_ANSWER]A[ b'Vasya and Digital Root' ].cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+#include<bitset>
+#include<string>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	int k;
+	char d;
+	cin>>k>>d;
+	cout<<d;
+	while(--k)cout<<0;
+	return 0;
+}
