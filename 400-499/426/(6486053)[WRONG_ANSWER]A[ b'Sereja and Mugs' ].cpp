d49727ccdf0b3062477c3d76dc723ba955d79a61
Yashar Shahi <yashar_sb_sb@yahoo.com>
2014-04-27 20:08:19 +0430

    submit 6486053 for 426/A/[Sereja and Mugs] with lang: GNU C++ & verdict: WRONG_ANSWER

---
+++ 400-499/426/(6486053)[WRONG_ANSWER]A[ b'Sereja and Mugs' ].cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+#include<bitset>
+#include<string>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	int n,m,s=0,inp,min=20;
+	cin>>n>>m;
+	for(int i = 0; i < n; ++i)
+	{
+		cin>>inp;
+		s+=inp;
+		if(min>inp)min=inp;
+	}
+	cout<<(s-min<=m?"YES":"NO");
+	return 0;
+}
