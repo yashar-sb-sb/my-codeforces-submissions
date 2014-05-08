f135971f0809d23ad0b974b0554c761f9979c141
Yashar Shahi <yashar_sb_sb@yahoo.com>
2014-05-08 09:20:59 +0430

    submit 6570473 for 216/A/[Tiling with Hexagons] with lang: GNU C++ & verdict: OK

---
+++ 200-299/216/(6570473)[OK]A[ b'Tiling with Hexagons' ].cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+#include<bitset>
+#include<string>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	int a,b,c;
+	cin>>a>>b>>c;
+	cout<<a*b+(a+b-1)*(c-1);
+	return 0;
+}
