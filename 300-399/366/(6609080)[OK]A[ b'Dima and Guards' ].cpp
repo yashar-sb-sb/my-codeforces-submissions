30b4946374c546de1b0c666c5de86c56b4ffd035
Yashar Shahi <yashar_sb_sb@yahoo.com>
2014-05-13 15:07:47 +0430

    submit 6609080 for 366/A/[Dima and Guards] with lang: GNU C++ & verdict: OK

---
+++ 300-399/366/(6609080)[OK]A[ b'Dima and Guards' ].cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+#include<bitset>
+#include<string>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	int n,i=4,a,b,c,d;
+	cin>>n;
+	while(i--)
+	{
+		cin>>a>>b>>c>>d;
+		if(min(a,b)+min(c,d)<=n){cout<<4-i<<' '<<min(a,b)<<' '<<n-min(a,b); return 0;}
+	}
+	cout<<-1;
+	return 0;
+}
