2069a3f5595153e818e481b7612a94731119b818
Yashar Shahi <yashar_sb_sb@yahoo.com>
2014-05-13 09:09:07 +0430

    submit 6607625 for 63/A/[Sinking Ship] with lang: GNU C++ & verdict: WRONG_ANSWER

---
+++ 0-99/63/(6607625)[WRONG_ANSWER]A[ b'Sinking Ship' ].cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+#include<bitset>
+#include<string>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	int n;
+	vector<string>r,wc,m,c;
+	cin>>n;
+	string s,t;
+	while(n--)
+	{
+		cin>>s>>t;
+		if(t == "rat")
+			r.push_back(s);
+		else if(t == "woman" || "child")
+			wc.push_back(s);
+		else if(t == "man")
+			m.push_back(s);
+		else
+			c.push_back(s);
+	}
+	vector<string>::iterator i;
+	for(i = r.begin(); i != r.end(); ++i)cout<<*i<<'\n';
+	for(i = wc.begin(); i != wc.end(); ++i)cout<<*i<<'\n';
+	for(i = m.begin(); i != m.end(); ++i)cout<<*i<<'\n';
+	for(i = c.begin(); i != c.end(); ++i)cout<<*i<<'\n';
+	return 0;
+}
