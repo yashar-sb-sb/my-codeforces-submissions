5266c6fa0b1eaa267470b3d19d41d4d230f888dc
Yashar Shahi <yashar_sb_sb@yahoo.com>
2014-05-21 19:21:59 +0430

    submit 6667760 for 402/A/[Nuts] with lang: GNU C++ & verdict: OK

---
+++ 400-499/402/(6667760)[OK]A[ b'Nuts' ].cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+#include<bitset>
+#include<string>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	int k, a, b, v;
+	cin>>k>>a>>b>>v;
+	a = a/v + (a%v?1:0);
+	v = a/k + (a%k?1:0);
+	cout<<((a-b>=b||(b<k&&a>b))?a-b:v+max(0,v-b));
+	return 0;
+}
