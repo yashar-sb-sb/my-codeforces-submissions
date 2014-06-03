71336130a2577fbad01e2c92e011ae82daa73cdb
Yashar Shahi <yashar_sb_sb@yahoo.com>
2014-06-03 21:50:45 +0430

    submit 6790929 for 440/A/[Forgotten Episode] with lang: GNU C++ & verdict: OK

---
+++ 400-499/440/(6790929)[OK]A[ b'Forgotten Episode' ].cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+#include<bitset>
+#include<string>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+bool A[100001];
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	LL n,i,t,s=0;
+	cin>>n;
+	t=n;
+	while(--t)
+	{
+		cin>>i;
+		s+=i;
+	}
+	cout<<n*(n+1)/2-s;
+	return 0;
+}
