55d2590c6eb8a33ddc12979a26af1777b4b0305e
Yashar Shahi <yashar_sb_sb@yahoo.com>
2014-06-12 14:48:27 +0430

    submit 6868120 for 430/A/[Points and Segments (easy)] with lang: GNU C++ & verdict: WRONG_ANSWER

---
+++ 400-499/430/(6868120)[WRONG_ANSWER]A[ b'Points and Segments (easy)' ].cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+#include<bitset>
+#include<string>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int A[100],I[100];
+
+bool f(int i, int j)
+{
+	return A[i]<A[j];
+}
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	int n,i;
+	cin>>n;
+	for(i = 0; i < n; ++i)
+	{
+		cin>>A[i];
+		I[i]=i;
+	}
+	sort(I,I+n,f);
+	i=n;
+	while(i--)A[I[i]] = i&1?0:1;
+	cout<<*A;
+	for(i = 1; i < n; ++i)
+	{
+		cout<<' '<<A[i];
+	}
+	return 0;
+}
