5cfc055200c4d649228f58862d0276951537ffeb
Yashar Shahi <yashar_sb_sb@yahoo.com>
2014-06-13 21:09:33 +0430

    submit 6880427 for 436/B/[Om Nom and Spiders] with lang: GNU C++ & verdict: TIME_LIMIT_EXCEEDED

---
+++ 400-499/436/(6880427)[TIME_LIMIT_EXCEEDED]B[ b'Om Nom and Spiders' ].cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+#include<bitset>
+#include<string>
+
+//#define Out(x,y) x<0 || y<0 || x>=n || y>=m
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int X[2000*2000],Y[2000*2000],dx[2000*2000],dy[2000*2000],res[2000];
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	int n,m,k,cs=0;
+	char ch;
+	cin>>n>>m>>k;
+	for(int i = 0; i < n; ++i)
+	{
+		for(int j = 0; j < m; ++j)
+		{
+			cin>>ch;
+			if(ch!='.')
+			{
+				X[cs]=i;
+				Y[cs]=j;
+				if(ch=='U')dx[cs]=-1;
+				else if(ch=='D')dx[cs]= 1;
+				else if(ch=='L')dy[cs]=-1;
+				else if(ch=='R')dy[cs]= 1;
+				++cs;
+			}
+		}
+	}
+	for(int i = 0; i <= n; ++i)
+	{
+		for(int j = 0; j < k; ++j)
+		{
+			bool tb = X[j]<0 || Y[j]<0 || X[j]>=n || Y[j]>=m;
+			if(X[j]==i && !tb)++res[Y[j]];
+		}
+		for(int j = 0; j < k; ++j)
+		{
+			X[j]+=dx[j];
+			Y[j]+=dy[j];
+		}
+	}
+	for(int i = 0; i < m; ++i)
+	{
+		cout<<res[i]<<' ';
+	}
+	return 0;
+}
