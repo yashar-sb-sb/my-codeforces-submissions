ea23688200119557fb6318638df11ff849a0e9db
Yashar Shahi <yashar_sb_sb@yahoo.com>
2014-07-06 18:01:05 +0430

    submit 7024524 for 445/B/[DZY Loves Chemistry] with lang: GNU C++ & verdict: WRONG_ANSWER

---
+++ 400-499/445/(7024524)[WRONG_ANSWER]B[ b'DZY Loves Chemistry' ].cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+#include<bitset>
+#include<string>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	LL d = 1;
+	LL n,m;
+	cin>>n>>m;
+	cout<<(d<<m);
+	return 0;
+}
