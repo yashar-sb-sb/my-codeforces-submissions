cb7b881c0302530059a1c571ec3822e2e5108f5c
Yashar Shahi <yashar_sb_sb@yahoo.com>
2014-07-13 18:04:33 +0430

    submit 7080416 for 447/C/[DZY Loves Sequences] with lang: GNU C++ & verdict: WRONG_ANSWER

---
+++ 400-499/447/(7080416)[WRONG_ANSWER]C[ b'DZY Loves Sequences' ].cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+#include<bitset>
+#include<string>
+#include<cstdio>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int A[100001];
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	int n,ma=1;
+	cin>>n;
+	for(int i = 0; i < n; ++i)cin>>A[i];
+	A[n] = 1e9+1;
+	for(int i = 1, cur = 0, curr = 0; i < n; ++i)
+	{
+		if(A[i]<=A[i-1])
+		{
+			if(A[i-1]+1<A[i+1]&&curr!=cur)
+			{
+				curr = cur;
+			}
+			cur = i;
+		}
+		ma = max(ma, i - curr + 1);
+	}
+	cout<<ma;
+	return 0;
+}
