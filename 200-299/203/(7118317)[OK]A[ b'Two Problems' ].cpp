e20da8fc55f075e5bad902424132190f05cebfb0
Yashar Shahi <yashar_sb_sb@yahoo.com>
2014-07-16 22:57:23 +0430

    submit 7118317 for 203/A/[Two Problems] with lang: GNU C++ & verdict: OK

---
+++ 200-299/203/(7118317)[OK]A[ b'Two Problems' ].cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+#include<bitset>
+#include<string>
+#include<cstdio>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	int x,t,a,b,da,db;
+	cin>>x>>t>>a>>b>>da>>db;
+	for(int i = 0; i < t; ++i)
+	{
+		for(int j = 0; j < t; ++j)
+		{
+			if(a-da*i+b-db*j==x||b-db*j==x||a-da*i==x||!x)
+			{
+				cout<<"YES";
+				return 0;
+			}
+		}
+	}
+	cout<<"NO";
+	return 0;
+}
