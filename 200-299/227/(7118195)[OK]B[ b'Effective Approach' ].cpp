684c6de3c504967d49cec0a4ad906e420922cc8e
Yashar Shahi <yashar_sb_sb@yahoo.com>
2014-07-16 22:30:23 +0430

    submit 7118195 for 227/B/[Effective Approach] with lang: GNU C++ & verdict: OK

---
+++ 200-299/227/(7118195)[OK]B[ b'Effective Approach' ].cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+#include<bitset>
+#include<string>
+#include<cstdio>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int A[100001];
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	LL l=0,r=0;
+	int n,m,q;
+	cin>>n;
+	for(int i = 0; i < n; ++i)
+	{
+		cin>>m;
+		A[m]=i;
+	}
+	cin>>m;
+	while(m--)
+	{
+		cin>>q;
+		l+=A[q]+1;
+		r+=n-A[q];
+	}
+	cout<<l<<' '<<r;
+	return 0;
+}
