7682f5a619508baf3e257537b3da8c810d7bfd41
Yashar Shahi <yashar_sb_sb@yahoo.com>
2014-07-19 20:33:14 +0430

    submit 7175762 for 450/C/[Jzzhu and Chocolate] with lang: GNU C++ & verdict: WRONG_ANSWER

---
+++ 400-499/450/(7175762)[WRONG_ANSWER]C[ b'Jzzhu and Chocolate' ].cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+#include<bitset>
+#include<string>
+#include<cstdio>
+
+#define mod 1000000007
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	LL n,x,y;
+	cin>>x>>y>>n;
+	switch(n%6)
+	{
+		case 0:cout<<((x-y)%mod+mod)%mod;break;
+		case 1:cout<<((x)%mod+mod)%mod;break;
+		case 2:cout<<((y)%mod+mod)%mod;break;
+		case 3:cout<<((y-x)%mod+mod)%mod;break;
+		case 4:cout<<((-x)%mod+mod)%mod;break;
+		case 5:cout<<((-y)%mod+mod)%mod;break;
+	}
+	return 0;
+}
