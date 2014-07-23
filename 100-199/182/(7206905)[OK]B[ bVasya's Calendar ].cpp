109da774864dc19ac2db8414afdb841970e7fc33
Yashar Shahi <yashar_sb_sb@yahoo.com>
2014-07-23 12:20:43 +0430

    submit 7206905 for 182/B/[Vasya's Calendar] with lang: GNU C++ & verdict: OK

---
+++ 100-199/182/(7206905)[OK]B[ bVasya's Calendar ].cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+#include<bitset>
+#include<string>
+#include<cstdio>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	int d,n,i,s=0;
+	cin>>d>>n;
+	while(--n)
+	{
+		cin>>i;
+		s+=d-i;
+	}
+	cout<<s;
+	return 0;
+}
