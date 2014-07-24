cabc60aa3483d0a55e633c8b049007e2d38ca753
Yashar Shahi <yashar_sb_sb@yahoo.com>
2014-07-24 20:20:10 +0430

    submit 7221432 for 451/A/[Game With Sticks] with lang: GNU C++ & verdict: OK

---
+++ 400-499/451/(7221432)[OK]A[ b'Game With Sticks' ].cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+#include<bitset>
+#include<string>
+#include<cstdio>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	int n,m;
+	cin>>n>>m;
+	cout<<(min(n,m)&1?"Akshat" : "Malvika");
+	return 0;
+}
