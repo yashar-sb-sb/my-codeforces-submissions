e91b9863589d5c7436f7cb3f5f1ef09469871651
Yashar Shahi <yashar_sb_sb@yahoo.com>
2014-08-01 18:57:44 +0430

    submit 7308192 for 374/B/[Inna and Nine] with lang: GNU C++ & verdict: WRONG_ANSWER

---
+++ 300-399/374/(7308192)[WRONG_ANSWER]B[ b'Inna and Nine' ].cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+#include<bitset>
+#include<string>
+#include<cstdio>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	char c,t=0;
+	LL s=1, a=0;
+	while(cin>>c)
+	{
+		if(c+t-'0'=='9')++a;
+		else
+		{
+			if(a>1&&a%2)s*=a-2;
+			a=0;
+		}
+		t=c;
+	}
+	if(a>1&&a%2)s*=a-2;
+	cout<<s;
+	return 0;
+}
