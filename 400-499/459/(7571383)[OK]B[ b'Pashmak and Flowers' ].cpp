63c3765fd55816c8ee726587fa2b0f196979228e
Yashar Shahi <yashar_sb_sb@yahoo.com>
2014-08-25 12:47:08 +0430

    submit 7571383 for 459/B/[Pashmak and Flowers] with lang: GNU C++ & verdict: OK

---
+++ 400-499/459/(7571383)[OK]B[ b'Pashmak and Flowers' ].cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+#include<bitset>
+#include<string>
+#include<cstdio>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	int n, i, mi=1e9+1, ma=0, mic, mac;
+	cin>>n;
+	while(n--)
+	{
+		cin>>i;
+		if(mi==i)++mic;
+		else if(mi>i)mi=i,mic=1;
+		if(ma==i)++mac;
+		else if(ma<i)ma=i,mac=1;
+	}
+	cout<<ma-mi<<' '<<(mi==ma?(LL)mac*(mac-1)/2:(LL)mic*mac);
+	return 0;
+}
