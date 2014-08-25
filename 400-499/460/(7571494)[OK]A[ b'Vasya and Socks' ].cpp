941f54d7fcfea04bf4ab3bb0825c8424ae71a076
Yashar Shahi <yashar_sb_sb@yahoo.com>
2014-08-25 13:06:55 +0430

    submit 7571494 for 460/A/[Vasya and Socks] with lang: GNU C++ & verdict: OK

---
+++ 400-499/460/(7571494)[OK]A[ b'Vasya and Socks' ].cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+#include<bitset>
+#include<string>
+#include<cstdio>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	int n,m;
+	cin>>n>>m;
+	int r = (n*m-1)/(m-1);
+	cout<<(r%m?r:r-1);
+	return 0;
+}
