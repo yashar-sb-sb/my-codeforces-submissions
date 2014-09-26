3383fe97a99cf8b234ee2bc734ba342e1647c48c
Yashar Shahi <yashar_sb_sb@yahoo.com>
2014-09-26 18:00:51 +0330

    submit 7957775 for 467/A/[George and Accommodation] with lang: GNU C++ & verdict: OK

---
+++ 400-499/467/(7957775)[OK]A[ b'George and Accommodation' ].cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+#include<bitset>
+#include<string>
+#include<cstdio>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	int n,p,q,c=0;
+	cin>>n;
+	while(n--)
+	{
+		cin>>p>>q;
+		if(p<q-1)++c;
+	}
+	cout<<c;
+	return 0;
+}
