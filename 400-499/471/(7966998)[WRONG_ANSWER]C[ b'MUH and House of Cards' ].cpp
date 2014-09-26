03efc444f60f36dcddb74109aba52a5473fdfb44
Yashar Shahi <yashar_sb_sb@yahoo.com>
2014-09-26 19:58:25 +0330

    submit 7966998 for 471/C/[MUH and House of Cards] with lang: GNU C++ & verdict: WRONG_ANSWER

---
+++ 400-499/471/(7966998)[WRONG_ANSWER]C[ b'MUH and House of Cards' ].cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+#include<bitset>
+#include<string>
+#include<cstdio>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	LL n,l=0,r=1000000000,m;
+	m=(l+r)/2;
+	cin>>n;
+	while(!(2*m+(m-1)*m/2*3<=n&&2*(m+1)+m*(m+1)/2*3>n))
+	{
+		if(2*m+(m-1)*m/2*3<=n)l=m;
+		else r = m+1;
+		m=(l+r)/2;
+	}
+	for(int i = 1; i < 3 && (n - 2*i)%3!=0; ++i)
+	{
+		--m;
+	}
+	if(m>0)cout<<m/3+1;
+	else cout<<0;
+	return 0;
+}
