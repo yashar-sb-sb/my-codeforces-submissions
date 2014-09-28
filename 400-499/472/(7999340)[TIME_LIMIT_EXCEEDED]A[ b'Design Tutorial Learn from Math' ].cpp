267b3373b5fe888f31d4e8c1dd70e613b78aa941
Yashar Shahi <yashar_sb_sb@yahoo.com>
2014-09-28 19:16:53 +0330

    submit 7999340 for 472/A/[Design Tutorial: Learn from Math] with lang: GNU C++ & verdict: TIME_LIMIT_EXCEEDED

---
+++ 400-499/472/(7999340)[TIME_LIMIT_EXCEEDED]A[ b'Design Tutorial Learn from Math' ].cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+#include<bitset>
+#include<string>
+#include<cstdio>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+bool prime(int n)
+{
+	for(int i = 2; i < sqrt(n)+1 && i < n; ++i)if(n%2 == 0)return false;
+	return true;
+}
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	int n;
+	cin>>n;
+	for(int i = 1; true; ++i)
+	{
+		if(!prime(i)&&!prime(n-i))
+		{
+			cout<<i<<' '<<n-i;
+			return 0;
+		}
+	}
+	return 0;
+}
