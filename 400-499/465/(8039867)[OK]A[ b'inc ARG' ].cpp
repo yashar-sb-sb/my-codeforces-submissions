82a9b110641ba02190f80da384c60b09abb70b50
Yashar Shahi <yashar_sb_sb@yahoo.com>
2014-09-30 22:13:13 +0330

    submit 8039867 for 465/A/[inc ARG] with lang: GNU C++ & verdict: OK

---
+++ 400-499/465/(8039867)[OK]A[ b'inc ARG' ].cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+#include<bitset>
+#include<string>
+#include<cstdio>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	int i=0,l;
+	string s;
+	cin>>l>>s;
+	l = s.size();
+	while(s[i++]=='1'&&i<l);
+	cout<<i;
+	return 0;
+}
