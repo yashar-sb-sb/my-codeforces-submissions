5dfe76bfa3579f82fd17139cc2a48a554cd727c8
Yashar Shahi <yashar_sb_sb@yahoo.com>
2014-10-01 11:44:24 +0330

    submit 8044362 for 125/A/[Measuring Lengths in Baden] with lang: GNU C++ & verdict: OK

---
+++ 100-199/125/(8044362)[OK]A[ b'Measuring Lengths in Baden' ].cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+#include<bitset>
+#include<string>
+#include<cstdio>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	int n;
+	cin>>n;
+	n=(n+1)/3;
+	cout<<n/12<<' '<<n%12;
+	return 0;
+}
