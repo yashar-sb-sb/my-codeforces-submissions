226e13024ee241ce5fe3266f735f773093300e49
Yashar Shahi <yashar_sb_sb@yahoo.com>
2014-10-12 19:09:35 +0330

    submit 8205056 for 476/C/[Dreamoon and Sums] with lang: GNU C++ & verdict: WRONG_ANSWER

---
+++ 400-499/476/(8205056)[WRONG_ANSWER]C[ b'Dreamoon and Sums' ].cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+#include<bitset>
+#include<string>
+#include<cstdio>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	LL a,b,mod = 10e9+7,res=0;
+	cin>>a>>b;
+	cout<<((((b*(b-1)/2)%mod)*((((b*a)%mod)*(a+1)/2)%mod+a)%mod)%mod);
+	return 0;
+}
