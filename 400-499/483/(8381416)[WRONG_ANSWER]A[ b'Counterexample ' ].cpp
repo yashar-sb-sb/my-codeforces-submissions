ccfe142cc131e298f279a5c8f6340e02d52ad2bb
Yashar Shahi <yashar_sb_sb@yahoo.com>
2014-10-24 20:42:13 +0330

    submit 8381416 for 483/A/[Counterexample ] with lang: GNU C++ & verdict: WRONG_ANSWER

---
+++ 400-499/483/(8381416)[WRONG_ANSWER]A[ b'Counterexample ' ].cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+#include<bitset>
+#include<string>
+#include<cstdio>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+LL gcd(int a, int b){return b?gcd(b,a%b):a;}
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	LL l,r,a,b,c;
+	cin>>l>>r;
+	for(a = l; a<=r; ++a)
+	{
+		for(b = a+1; b<=r; ++b)
+		{
+			for(c = b+1; c<=r; ++c)
+			{
+				if(gcd(b,a) == 1 && gcd(c,b) == 1 && gcd(c,a) != 1){cout<<a<<' '<<b<<' '<<c; return 0;}
+			}
+		}
+	}
+	cout<<-1;
+	return 0;
+}
