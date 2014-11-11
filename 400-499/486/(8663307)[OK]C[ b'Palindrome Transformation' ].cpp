5b9426dc10aaf099654475945e5acde12e26a8a5
Yashar Shahi <yashar_sb_sb@yahoo.com>
2014-11-11 21:43:22 +0330

    submit 8663307 for 486/C/[Palindrome Transformation] with lang: GNU C++ & verdict: OK

---
+++ 400-499/486/(8663307)[OK]C[ b'Palindrome Transformation' ].cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+#include<bitset>
+#include<string>
+#include<cstdio>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+char A[100000];
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	int n,p,m=0,d1=-1,d2=0;
+	cin>>n>>p;
+	--p;
+	for(int i = 0; i < n; ++i){cin>>A[i];A[i]-='a';};
+	for(int i = 0; i < n/2; ++i)
+	{
+		if(A[i] != A[n-i-1])d2=i;
+		if(d1==-1 && A[i] != A[n-i-1])d1=i;
+		m+=min(abs(A[i] - A[n-i-1]),26-abs(A[i] - A[n-i-1]));
+	}
+	if(p>=n/2)p = n-p-1;
+	if(m==0)cout<<m;
+	else if(p<=d1)cout<<m+d2-p;
+	else if(p>=d2)cout<<m+p-d1;
+	else cout<<m+d2-d1+min(p-d1,d2-p);
+	return 0;
+}
