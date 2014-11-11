2172a087db490b5966a75fd7bed522b666076cfe
Yashar Shahi <yashar_sb_sb@yahoo.com>
2014-11-11 19:08:37 +0330

    submit 8655167 for 486/A/[Calculating Function] with lang: GNU C++ & verdict: OK

---
+++ 400-499/486/(8655167)[OK]A[ b'Calculating Function' ].cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+#include<bitset>
+#include<string>
+#include<cstdio>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	LL n;
+	cin>>n;
+	cout<<n/2-(n&1)*n;
+	return 0;
+}
