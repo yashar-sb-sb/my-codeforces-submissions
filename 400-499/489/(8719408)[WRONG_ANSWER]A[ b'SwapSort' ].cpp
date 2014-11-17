ebb578a4147ebfebb35807126f53597ef172eb47
Yashar Shahi <yashar_sb_sb@yahoo.com>
2014-11-17 19:09:40 +0330

    submit 8719408 for 489/A/[SwapSort] with lang: GNU C++ & verdict: WRONG_ANSWER

---
+++ 400-499/489/(8719408)[WRONG_ANSWER]A[ b'SwapSort' ].cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+#include<bitset>
+#include<string>
+#include<cstdio>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+pii A[3000];
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	int n;
+	cin>>n;
+	for(int i = 0; i < n; ++i)
+	{
+		cin>>A[i].first;
+		A[i].second = i;
+	}
+	sort(A,A+n);
+	cout<<n<<'\n';
+	for(int i = 0; i < n; ++i)
+	{
+		cout<<A[i].second<<' '<<i<<'\n';
+	}
+	return 0;
+}
