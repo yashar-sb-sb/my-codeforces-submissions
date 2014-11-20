cb809981058ba7e0e01f018a113aac6f215ed454
Yashar Shahi <yashar_sb_sb@yahoo.com>
2014-11-20 17:22:46 +0330

    submit 8764972 for 462/B/[Appleman and Card Game] with lang: GNU C++ & verdict: OK

---
+++ 400-499/462/(8764972)[OK]B[ b'Appleman and Card Game' ].cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+#include<bitset>
+#include<string>
+#include<cstdio>
+#include<list>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int A[26];
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	int k;
+	LL s=0;
+	char c;
+	cin>>k>>k;
+	while(cin>>c)++A[c-'A'];
+	sort(A,A+26);
+	int i = 25;
+	while(k)
+	{
+		s+=(LL)min(k,A[i])*min(k,A[i]);
+		k-=min(A[i--],k);
+	}
+	cout<<s;
+	return 0;
+}
