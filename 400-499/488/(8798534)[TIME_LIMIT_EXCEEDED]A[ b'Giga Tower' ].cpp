b283b108c46c0516daee831394b7c73e49eccd31
Yashar Shahi <yashar_sb_sb@yahoo.com>
2014-11-22 14:14:27 +0330

    submit 8798534 for 488/A/[Giga Tower] with lang: GNU C++ & verdict: TIME_LIMIT_EXCEEDED

---
+++ 400-499/488/(8798534)[TIME_LIMIT_EXCEEDED]A[ b'Giga Tower' ].cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+#include<bitset>
+#include<string>
+#include<cstdio>
+#include<list>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	int a,i,t;
+	cin>>a;
+	++a;
+	for(i = 1; true; ++i, ++a)
+	{
+		t = a;
+		while(t && t%10!=8)t/=10;
+		if(t%10==8)break;
+	}
+	cout<<i;
+	return 0;
+}
