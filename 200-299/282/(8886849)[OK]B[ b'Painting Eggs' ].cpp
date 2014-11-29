bd3b306ed9d6078ce0ca3c2703247af5bdea280b
Yashar Shahi <yashar_sb_sb@yahoo.com>
2014-11-29 13:12:21 +0330

    submit 8886849 for 282/B/[Painting Eggs] with lang: GNU C++ & verdict: OK

---
+++ 200-299/282/(8886849)[OK]B[ b'Painting Eggs' ].cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+#include<bitset>
+#include<string>
+#include<cstdio>
+#include<list>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+//ios_base::sync_with_stdio(0);
+	int n,a,b,s = 0;
+	scanf("%d",&n);
+	while(n--)
+	{
+		scanf("%d%d",&a,&b);
+		if(s+a<=500)
+		{
+			s+=a;
+			putchar('A');
+		}
+		else
+		{
+			s-=b;
+			putchar('G');
+		}
+	}
+	return 0;
+}
