4455c337260f039ff9cea3a030b9bccf2e88c7e3
Yashar Shahi <yashar_sb_sb@yahoo.com>
2014-12-20 15:32:02 +0330

    submit 9206074 for 495/A/[Digital Counter] with lang: MS C# & verdict: COMPILATION_ERROR

---
+++ 400-499/495/(9206074)[COMPILATION_ERROR]A[ b'Digital Counter' ].cs
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+#include<bitset>
+#include<string>
+#include<cstdio>
+#include<list>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int A[10]={2,6,2,3,2,3,2,5,1,2};
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	int n;
+	cin>>n;
+	cout<<A[n%10]*A[n/10];
+	return 0;
+}
