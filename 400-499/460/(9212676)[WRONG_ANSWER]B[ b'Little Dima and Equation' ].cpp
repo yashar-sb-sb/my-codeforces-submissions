22f927acaf1c7918cf40aa989dbcc5cfb7a21c68
Yashar Shahi <yashar_sb_sb@yahoo.com>
2014-12-21 08:54:29 +0330

    submit 9212676 for 460/B/[Little Dima and Equation] with lang: GNU C++ & verdict: WRONG_ANSWER

---
+++ 400-499/460/(9212676)[WRONG_ANSWER]B[ b'Little Dima and Equation' ].cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+#include<bitset>
+#include<string>
+#include<cstdio>
+#include<list>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int A[81];
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	int a, b, c, t=0, x, l, k;
+	cin>>a>>b>>c;
+	for(int i = 1; i < 82; ++i)
+	{
+		x = i;
+		for(int j = 1; j < a; ++j)x*=i;
+		x = b*x+c;
+		l = x;
+		k = 0;
+		while(l)
+		{
+			k+=l%10;
+			l/=10;
+		}
+		if(k==i)A[t++]=x;
+	}
+	cout<<t<<'\n';
+	for(int i = 0; i < t; ++i)cout<<A[i]<<' ';
+	return 0;
+}
