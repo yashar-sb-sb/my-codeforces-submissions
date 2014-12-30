902a34b9410a2664dc9beb40b998c2de6fdaf4af
Yashar Shahi <yashar_sb_sb@yahoo.com>
2014-12-30 19:29:05 +0330

    submit 9316941 for 500/C/[New Year Book Reading] with lang: GNU C++ & verdict: OK

---
+++ 500-599/500/(9316941)[OK]C[ b'New Year Book Reading' ].cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+#include<bitset>
+#include<string>
+#include<cstdio>
+#include<list>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int A[501], F[501], B[1000];
+vector<int> s,st;
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	LL n,m,t;
+	cin>>n>>m;
+	for(int i = 1; i <= n; ++i)cin>>A[i];
+	for(int i = 0; i < m; ++i)
+	{
+		cin>>B[i];
+	}
+	for(int i = 0; i < m; ++i)
+	{
+		if(!F[B[i]])
+		{
+			s.push_back(B[i]);
+		}
+		F[B[i]] = true;
+	}
+	reverse(s.begin(),s.end());
+	n = 0;
+	for(int i = 0; i < m; ++i)
+	{
+		t = 0;
+		while(s.back()!=B[i])
+		{
+			st.push_back(s.back());
+			t+=A[s.back()];
+			s.pop_back();
+		}
+		n += t;
+		t = s.back();
+		s.pop_back();
+		while(!st.empty())
+		{
+			s.push_back(st.back());
+			st.pop_back();
+		}
+		s.push_back(t);
+	}
+	cout<<n;
+	return 0;
+}
