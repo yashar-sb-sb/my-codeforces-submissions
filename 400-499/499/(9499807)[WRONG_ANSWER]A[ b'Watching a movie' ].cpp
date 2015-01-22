9d4fb621b0d944b14145f52c23518f8442befddc
Yashar Shahi <yashar_sb_sb@yahoo.com>
2015-01-22 11:42:27 +0330

    submit 9499807 for 499/A/[Watching a movie] with lang: GNU C++ & verdict: WRONG_ANSWER

---
+++ 400-499/499/(9499807)[WRONG_ANSWER]A[ b'Watching a movie' ].cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+#include<bitset>
+#include<string>
+#include<cstdio>
+#include<list>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	int n,x,tr=0,res=0,l,r;
+	cin>>n>>x;
+	for(int i = 0; i < n; ++i)
+	{
+		cin>>l>>r;
+		res+=(l-tr-1)%3+r-l+1;
+	}
+	cout<<res;
+	return 0;
+}
