cbce5803ac6ea486a01da796faf57e781bc9ade3
Yashar Shahi <yashar_sb_sb@yahoo.com>
2015-01-27 20:16:47 +0330

    submit 9576438 for 508/B/[Anton and currency you all know] with lang: GNU C++ & verdict: WRONG_ANSWER

---
+++ 500-599/508/(9576438)[WRONG_ANSWER]B[ b'Anton and currency you all know' ].cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+#include<bitset>
+#include<string>
+#include<cstdio>
+#include<list>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	string s;
+	cin>>s;
+	int l = s.size(),i;
+	for(i = 0; i < l && (s[i]-'0')&1; ++i)l;
+	for(int j = i; j < l; ++j)
+	{
+		if((s[j]-'0')&1 == 0)
+		{
+			if(s[j] < s[l-1])
+			{
+				swap(s[j],s[l-1]);
+				cout<<s;
+				return 0;
+			}
+			i = j;
+		}
+	}
+	if(i < l)
+	{
+		swap(s[i],s[l-1]);
+		cout<<s;
+	}
+	else cout<<-1;
+	return 0;
+}
