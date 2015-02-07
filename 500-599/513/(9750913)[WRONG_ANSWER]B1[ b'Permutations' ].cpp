6f3fa96cb448d0aa0625172818e878fe800986a6
Yashar Shahi <yashar_sb_sb@yahoo.com>
2015-02-07 20:58:09 +0330

    submit 9750913 for 513/B1/[Permutations] with lang: GNU C++ & verdict: WRONG_ANSWER

---
+++ 500-599/513/(9750913)[WRONG_ANSWER]B1[ b'Permutations' ].cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+#include<bitset>
+#include<string>
+#include<cstdio>
+#include<list>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int n,m,A[6];
+
+int f()
+{
+	int s = 0;
+	for(int i = 0; i < n; ++i)
+	{
+		for(int j = i; j < n; ++j)
+		{
+			int re = 6;
+			for(int k = i; k <= j; ++k)
+			{
+				re = min(re, A[k]);
+			}
+			s+=re;
+		}
+	}
+	return s;
+}
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	cin>>n>>m;
+	int ma = 0, cn=0;
+	for(int i = 0; i < n; ++i)
+	{
+		A[i] = i + 1;
+	}
+	do
+	{
+		int t = f();
+		if(t>ma)cn=1, ma = t;
+		else if(t == ma)++cn;
+		
+		if(cn == m)break;
+	} while ( std::next_permutation(A,A+n) );
+	
+	for(int i = 0; i < n; ++i)cout<<A[i]<<' ';
+	return 0;
+}
