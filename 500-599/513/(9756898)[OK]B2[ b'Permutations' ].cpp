d61e84fd79af9f3fb594d329eb23d5141b47ccb3
Yashar Shahi <yashar_sb_sb@yahoo.com>
2015-02-07 21:52:50 +0330

    submit 9756898 for 513/B2/[Permutations] with lang: GNU C++ & verdict: OK

---
+++ 500-599/513/(9756898)[OK]B2[ b'Permutations' ].cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+#include<bitset>
+#include<string>
+#include<cstdio>
+#include<list>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+LL n,m,A[50];
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	cin>>n>>m;
+	LL ma = 1LL<<(n-2), cn=0,l =0, r=n-1;--m;
+	for(int i = 0; i < n; ++i)
+	{
+		if(ma > m)
+		{
+			A[l++] = i + 1;
+		}
+		else
+		{
+			A[r--] = i + 1;
+			m -= ma;
+		}
+		ma = ma>>1;
+	}
+	
+	for(int i = 0; i < n; ++i)cout<<A[i]<<' ';
+	return 0;
+}
