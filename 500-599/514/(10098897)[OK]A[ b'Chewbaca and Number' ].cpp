a6aa3ed60eafce75378e01844c05dfdff79e93e9
Yashar Shahi <yashar_sb_sb@yahoo.com>
2015-03-01 20:25:25 +0330

    submit 10098897 for 514/A/[Chewbaсca and Number] with lang: GNU C++ & verdict: OK

---
+++ 500-599/514/(10098897)[OK]A[ b'Chewbaca and Number' ].cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+#include<bitset>
+#include<string>
+#include<cstdio>
+#include<list>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	LL x,res = 0, c=1;
+	cin>>x;
+	while(x/10)
+	{
+		res += min(x%10,9-x%10)*c;
+		c*=10;
+		x/=10;
+	}
+	res += (min(x%10,9-x%10)?min(x%10,9-x%10):9)*c;
+	cout<<res;
+	return 0;
+}
