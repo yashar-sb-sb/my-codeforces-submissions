04bdf53b86130d7d94978543349124bed2055cde
Yashar Shahi <yashar_sb_sb@yahoo.com>
2015-03-30 23:17:29 +0430

    submit 10527771 for 519/C/[A and B and Team Training] with lang: GNU C++ & verdict: OK

---
+++ 500-599/519/(10527771)[OK]C[ b'A and B and Team Training' ].cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+#include<bitset>
+#include<string>
+#include<cstdio>
+#include<list>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	int n,m;
+	cin>>n>>m;
+	if(n>m)swap(n,m);
+	if(n+n>m)cout<<(n+m)/3;
+	else cout<<n;
+	return 0;
+}
