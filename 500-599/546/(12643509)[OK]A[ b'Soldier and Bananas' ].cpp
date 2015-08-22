b30d18fed05b18fbb99ebe861c25a39aed540629
Yashar Shahi <yashar_sb_sb@yahoo.com>
2015-08-22 18:41:39 +0430

    submit 12643509 for 546/A/[Soldier and Bananas] with lang: GNU C++ & verdict: OK

---
+++ 500-599/546/(12643509)[OK]A[ b'Soldier and Bananas' ].cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+#include<bitset>
+#include<string>
+#include<cstdio>
+#include<list>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	LL k,n,w;
+	cin>>k>>n>>w;
+	cout<<max(0LL,w*(w+1)/2*k-n);
+	return 0;
+}
