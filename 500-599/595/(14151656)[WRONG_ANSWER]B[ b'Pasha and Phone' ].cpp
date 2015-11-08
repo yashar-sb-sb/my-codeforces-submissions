539eb3766128eafc8e2807171a63848a0734543b
Yashar Shahi <yashar_sb_sb@yahoo.com>
2015-11-08 20:55:02 +0330

    submit 14151656 for 595/B/[Pasha and Phone] with lang: GNU C++ & verdict: WRONG_ANSWER

---
+++ 500-599/595/(14151656)[WRONG_ANSWER]B[ b'Pasha and Phone' ].cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+#include<bitset>
+#include<string>
+#include<cstdio>
+#include<list>
+#include<set>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int A[100000],B[100000];
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	int n,k,res=1,l;
+	cin>>n>>k;
+	l = 1;
+	for(int i = 0; i < k; ++i)l *= 10;
+	for(int i = 0; i < n/k; ++i)cin>>A[i];
+	for(int i = 0; i < n/k; ++i)cin>>B[i];
+	
+	for(int i = 0; i < n/k; ++i)
+	{
+		int a = (l-1)/A[i]+1;
+		a -= (l/10*(1+B[i])-1)/A[i];
+		a += (l/10*B[i]-1)/A[i];
+		res = res * a % (int)(1e9+7);
+	}
+	cout<<res;
+	return 0;
+}
