5bf9ea58ae156fdefdc6b22a84b0568c84669de7
Yashar Shahi <yashar_sb_sb@yahoo.com>
2015-11-14 02:04:12 +0330

    submit 14245964 for 598/C/[Nearest vectors] with lang: GNU C++ & verdict: WRONG_ANSWER

---
+++ 500-599/598/(14245964)[WRONG_ANSWER]C[ b'Nearest vectors' ].cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+#include<bitset>
+#include<string>
+#include<cstdio>
+#include<list>
+#include<set>
+#include<limits>
+
+
+using namespace std;
+
+
+struct paii
+{
+	long double first;
+	long double second;
+	int ind;
+};
+
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef struct paii pii;
+
+pii A[100000];
+
+int ff(pii i)
+{
+	if(i.second == 0 && i.first > 0)return 0;
+	if(i.second > 0 && i.first > 0)return 1;
+	if(i.second > 0 && i.first == 0)return 2;
+	if(i.second > 0 && i.first < 0)return 3;
+	if(i.second == 0 && i.first < 0)return 4;
+	if(i.second < 0 && i.first < 0)return 5;
+	if(i.second < 0 && i.first == 0)return 6;
+	return 7;
+}
+
+bool f(pii i, pii j)
+{
+	int a = ff(i), b = ff(j);
+	if(a < b)return true;
+	if(b < a)return false;
+	return i.first * j.second - j.first * i.second > 0;
+}
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	int n,ind=0;
+	cin>>n;
+	for(int i = 0; i < n; ++i){cin>>A[i].first>>A[i].second;A[i].ind = i+1;}
+	sort(A+0,A+n,f);
+	ldb res = -numeric_limits<ldb>::infinity(), temp,temp1;
+	for(int i = 0; i < n; ++i)
+	{
+		temp = (A[i].first*A[(i+1)%n].first+A[i].second*A[(i+1)%n].second);
+		temp1 = (ldb)sqrt(A[i].first*A[i].first+A[i].second*A[i].second)*sqrt(A[(i+1)%n].first*A[(i+1)%n].first+A[(i+1)%n].second*A[(i+1)%n].second);
+		temp /= temp1;
+		if(temp > res){ind = i; res = temp; }
+	}
+	cout<<A[ind].ind<<' '<<A[(ind+1)%n].ind;
+	return 0;
+}
