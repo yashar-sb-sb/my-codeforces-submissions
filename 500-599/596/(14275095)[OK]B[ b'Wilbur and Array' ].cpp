d55c7199b0651288669e94327feebe9c9a892107
Yashar Shahi <yashar_sb_sb@yahoo.com>
2015-11-15 20:20:48 +0330

    submit 14275095 for 596/B/[Wilbur and Array] with lang: GNU C++ & verdict: OK

---
+++ 500-599/596/(14275095)[OK]B[ b'Wilbur and Array' ].cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+#include<bitset>
+#include<string>
+#include<cstdio>
+#include<list>
+#include<set>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	LL n,s=0,t=0,i;
+	cin>>n;
+	while(n--)
+	{
+		cin>>i;
+		s+=abs(i-t);
+		t = i;
+	}
+	cout<<s;
+	return 0;
+}
