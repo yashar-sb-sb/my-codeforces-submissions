f85db4f3ba740d526ce5a702a093f30a9b1a3c63
Yashar Shahi <yashar_sb_sb@yahoo.com>
2015-11-25 13:52:40 +0330

    submit 14467134 for 591/A/[Wizards' Duel] with lang: GNU C++ & verdict: OK

---
+++ 500-599/591/(14467134)[OK]A[ bWizards' Duel ].cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+#include<bitset>
+#include<string>
+#include<cstdio>
+#include<list>
+#include<set>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+ldb n,a,b;
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	cin>>n>>a>>b;
+	cout<<n*(a/(a+b));
+	return 0;
+}
