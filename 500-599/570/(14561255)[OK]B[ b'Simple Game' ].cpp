0d9311db8c8c0b1bd4764e424f7cd1476a1af1a3
Yashar Shahi <yashar_sb_sb@yahoo.com>
2015-11-29 21:05:37 +0330

    submit 14561255 for 570/B/[Simple Game] with lang: GNU C++ & verdict: OK

---
+++ 500-599/570/(14561255)[OK]B[ b'Simple Game' ].cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+#include<bitset>
+#include<string>
+#include<cstdio>
+#include<list>
+#include<set>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	int n, m;
+	cin>> n >> m;
+	cout<<max(1,m-1>=n-m?m-1:m+1);
+	return 0;
+}
