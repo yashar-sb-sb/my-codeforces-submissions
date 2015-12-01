784bea5ed203b8e4209f6e633e76b14bd7556224
Yashar Shahi <yashar_sb_sb@yahoo.com>
2015-12-01 16:34:54 +0330

    submit 14584470 for 546/B/[Soldier and Badges] with lang: GNU C++ & verdict: WRONG_ANSWER

---
+++ 500-599/546/(14584470)[WRONG_ANSWER]B[ b'Soldier and Badges' ].cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+#include<bitset>
+#include<string>
+#include<cstdio>
+#include<list>
+#include<set>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int A[3001];
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	int n, res=0;
+	cin>>n;
+	stack<pii> s;
+	for(int i = 1; i <= n; ++i)cin>>A[i];
+	sort(A+1,A+n+1);
+	s.push(make_pair<int,int>(A[n],1e9));
+	for(int i = n; i > 0; --i)
+	{
+		if(A[i] == A[i-1])
+		{
+			res += s.top().first - A[i];
+			++s.top().first;
+			if(s.top().first == s.top().second)s.pop();
+		}
+		else if(A[i] > A[i-1] + 1)
+			s.push(make_pair<int,int>(A[i-1]+1,A[i]));
+	}
+	cout<<res;
+	return 0;
+}
