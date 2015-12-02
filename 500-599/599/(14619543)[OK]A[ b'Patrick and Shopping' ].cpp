adce930a35f86fd1c462b09afd5e7b5dbed17e92
Yashar Shahi <yashar_sb_sb@yahoo.com>
2015-12-02 16:42:13 +0330

    submit 14619543 for 599/A/[Patrick and Shopping] with lang: GNU C++ & verdict: OK

---
+++ 500-599/599/(14619543)[OK]A[ b'Patrick and Shopping' ].cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+#include<bitset>
+#include<string>
+#include<cstdio>
+#include<list>
+#include<set>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	int a,b,c;
+	cin>>a>>b>>c;
+	cout<<min(a+b+c,min(a+a+b+b,min(a+c+a+c,b+c+b+c)));
+	return 0;
+}
