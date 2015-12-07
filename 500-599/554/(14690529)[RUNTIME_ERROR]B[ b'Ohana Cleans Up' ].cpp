7e0cb1e1f4c93af297ee6d360c15d26eb0e24ea2
Yashar Shahi <yashar_sb_sb@yahoo.com>
2015-12-07 22:25:12 +0330

    submit 14690529 for 554/B/[Ohana Cleans Up] with lang: GNU C++ & verdict: RUNTIME_ERROR

---
+++ 500-599/554/(14690529)[RUNTIME_ERROR]B[ b'Ohana Cleans Up' ].cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+#include<bitset>
+#include<string>
+#include<cstdio>
+#include<list>
+#include<set>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+struct node
+{
+	struct node *left,*right;
+	int c;
+	node()
+	{
+		c = 0;
+	}
+}root;
+
+int m = 0;
+
+void insert(struct node *c, string *s, int l = 1)
+{
+	if((*s).size() == l){m = max(m,c->c); return;};
+	if((*s)[l] == (*s)[0])
+	{
+		if(!c->left)c->left = new struct node;
+		++c->left->c;
+		insert(c->left,s,l+1);
+	}
+	else
+	{
+		if(!c->right)c->right = new struct node;
+		++c->right->c;
+		insert(c->right,s,l+1);
+	}
+}
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	int n;
+	string s;
+	cin>>n;
+	while(n--){cin>>s;insert(&root,&s);}
+	cout<<m;
+	return 0;
+}
