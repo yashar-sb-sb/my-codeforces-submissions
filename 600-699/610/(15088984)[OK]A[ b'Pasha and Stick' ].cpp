56c3cfcd08921b5f80b258be70d845262ac080d8
Yashar Shahi <yashar_sb_sb@yahoo.com>
2015-12-29 22:06:29 +0330

    submit 15088984 for 610/A/[Pasha and Stick] with lang: GNU C++ & verdict: OK

---
+++ 600-699/610/(15088984)[OK]A[ b'Pasha and Stick' ].cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+#include<bitset>
+#include<string>
+#include<cstdio>
+#include<list>
+#include<set>
+#include<fstream>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	int n;
+	cin>>n;
+	if(n&1){cout<<0;return 0;}
+	n = n>>1;
+	cout<<(n>>1) - !(n&1);
+	return 0;
+}
