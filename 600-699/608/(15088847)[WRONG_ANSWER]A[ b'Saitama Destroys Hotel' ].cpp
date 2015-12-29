23c99221ca3854d4ecec271909f9410bc59f1c47
Yashar Shahi <yashar_sb_sb@yahoo.com>
2015-12-29 21:56:38 +0330

    submit 15088847 for 608/A/[Saitama Destroys Hotel] with lang: GNU C++ & verdict: WRONG_ANSWER

---
+++ 600-699/608/(15088847)[WRONG_ANSWER]A[ b'Saitama Destroys Hotel' ].cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<sstream>
+#include<iomanip>
+#include<bitset>
+#include<string>
+#include<cstdio>
+#include<list>
+#include<set>
+#include<fstream>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int A[1002];
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	int n, s, a, b;
+	cin>> n >> s;
+	for(int i = 0; i < n; ++i)
+	{
+		cin>>a>>b;
+		A[a] = max(A[a], b);
+	}
+	for(int i = s; i > -1; --i)
+	{
+		A[i] = max(A[i], A[i+1] + 1);
+	}
+	cout<<*A;
+	return 0;
+}
