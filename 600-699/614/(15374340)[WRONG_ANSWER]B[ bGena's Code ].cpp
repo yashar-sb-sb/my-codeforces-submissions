59e489c9c5bdc7c31af182d6e06c202ad91890ca
Yashar Shahi <yashar_sb_sb@yahoo.com>
2016-01-14 22:58:37 +0330

    submit 15374340 for 614/B/[Gena's Code] with lang: GNU C++ & verdict: WRONG_ANSWER

---
+++ 600-699/614/(15374340)[WRONG_ANSWER]B[ bGena's Code ].cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+string S[100000];
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	int n, c = 0;
+	cin>>n;
+	for(int i = 0; i < n; ++i)
+	{
+		cin>>S[i];
+		if(S[i] == "0"){cout<<0; return 0;}
+		c = 0;
+		for(string::iterator ii = S[i].begin(); ii != S[i].end(); ++ii)
+		{
+			if(*ii >= '1')
+			{
+				++c;
+			}
+		}
+		if(c>1)swap(S[i], S[0]);
+	}
+	cout<<S[0];
+	for(int i = 1; i < n; ++i)for(int j = 1; j < S[i].size(); ++j)cout<<'0';
+	return 0;
+}
