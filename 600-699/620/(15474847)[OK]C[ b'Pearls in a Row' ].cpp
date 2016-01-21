023a559e2183ad4b4eb15693d7815f6d11528ebb
Yashar Shahi <yashar_sb_sb@yahoo.com>
2016-01-21 18:57:50 +0330

    submit 15474847 for 620/C/[Pearls in a Row] with lang: GNU C++ & verdict: OK

---
+++ 600-699/620/(15474847)[OK]C[ b'Pearls in a Row' ].cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+set<int> s;
+vector<pii> res;
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	int n, a, start = 1;
+	cin>>n;
+	for(int i = 1; i <= n; ++i)
+	{
+		cin>>a;
+		if(s.find(a)!=s.end())
+		{
+			res.push_back(pii(start,i));
+			start = i + 1;
+			s.clear();
+		}
+		else s.insert(a);
+	}
+	cout<<(res.size()?(int)res.size():-1)<<'\n';
+	if(res.size())
+	{
+		res.back().second = max(res.back().second, n);
+	}
+	for(vector<pii>::iterator i = res.begin(); i != res.end(); ++i)
+	{
+		cout<<i->first<<' '<<i->second<<'\n';
+	}
+	return 0;
+}
