87df182dbe9842878ad6d11057b5ed9be5bff5b0
Yashar Shahi <yashar_sb_sb@yahoo.com>
2016-01-24 22:21:24 +0330

    submit 15561505 for 617/A/[Elephant] with lang: GNU C++ & verdict: OK

---
+++ 600-699/617/(15561505)[OK]A[ b'Elephant' ].cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	int x;
+	cin>>x;
+	cout<<(x+4)/5;
+	return 0;
+}
