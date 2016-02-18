f0f760afa1a53b3bd5cb6f8d29844ab42ac1ff04
Yashar Shahi <yashar_sb_sb@yahoo.com>
2016-02-18 18:43:07 +0330

    submit 16146973 for 630/B/[Moore's Law] with lang: GNU C++ & verdict: OK

---
+++ 600-699/630/(16146973)[OK]B[ bMoore's Law ].cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	ldb a, b, c=1.000000011;
+	cin>>a>>b;
+	cout.precision(12);
+	cout<<a*pow(c,b);
+	return 0;
+}
