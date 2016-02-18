e32f453c56ec78f932a7e27d8ad01b590cbe3744
Yashar Shahi <yashar_sb_sb@yahoo.com>
2016-02-18 20:07:07 +0330

    submit 16169614 for 630/R/[Game] with lang: GNU C++ & verdict: OK

---
+++ 600-699/630/(16169614)[OK]R[ b'Game' ].cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	LL n,i=1;
+	cin>>n;
+	cout<<2 - (n&i);
+	return 0;
+}
