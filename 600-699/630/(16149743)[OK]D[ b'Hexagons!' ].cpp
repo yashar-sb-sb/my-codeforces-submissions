dd74caa4909588fe8c730b1a01d97e49985e1042
Yashar Shahi <yashar_sb_sb@yahoo.com>
2016-02-18 18:53:29 +0330

    submit 16149743 for 630/D/[Hexagons!] with lang: GNU C++ & verdict: OK

---
+++ 600-699/630/(16149743)[OK]D[ b'Hexagons!' ].cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	uLL n;
+	cin>>n;
+	cout<<3*n*(n+1)+1;
+	return 0;
+}
