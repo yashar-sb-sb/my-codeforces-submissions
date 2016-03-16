a3f2b60735d00b8aca4d18fba2a4e13585bc1316
Yashar Shahi <yashar_sb_sb@yahoo.com>
2016-03-16 13:36:14 +0330

    submit 16741203 for 644/A/[Parliament of Berland] with lang: PyPy 3 & verdict: SKIPPED

---
+++ 600-699/644/(16741203)[SKIPPED]A[ b'Parliament of Berland' ].py
@@ -0,0 +1,21 @@
+[n,a,b] = list(map(int,input().split(' ')))
+
+r = [(b+1)*[0] for _ in range(a+1)]
+k = 1
+t = [1]
+for i in range(1,a+1):
+    for j in range(1,b+1):
+        if k < n :
+            t.append(k+1)
+            k += 1
+        for it in t:
+            if (r[i-1][j] == 0 or r[i-1][j]%2 != it%2) and (r[i][j-1] == 0 or r[i][j-1]%2 != it%2):
+                r[i][j] = it
+                break
+        if r[i][j] != 0:
+            t.remove(r[i][j])
+if k == n and len(t) == 0:
+    for i in r[1:]:
+        print(*i[1:])
+else:
+    print(-1)
