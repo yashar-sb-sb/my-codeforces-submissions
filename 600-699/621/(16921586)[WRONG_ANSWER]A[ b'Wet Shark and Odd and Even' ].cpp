8a518f80d1373f4e077be48399e77953d9a1a6a7
Yashar Shahi <yashar_sb_sb@yahoo.com>
2016-03-25 16:55:41 +0430

    submit 16921586 for 621/A/[Wet Shark and Odd and Even] with lang: GNU C++ & verdict: WRONG_ANSWER

---
+++ 600-699/621/(16921586)[WRONG_ANSWER]A[ b'Wet Shark and Odd and Even' ].cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	int n, m, a;
+	cin>>n;
+	LL r = 0;
+	for(int i = 0; i < n; ++i)
+	{
+		cin>>a;
+		if(a&1)m = m ? min(m,a): a;
+		r += a;
+	}
+	cout<< r - (r&1)*m;
+	return 0;
+}
