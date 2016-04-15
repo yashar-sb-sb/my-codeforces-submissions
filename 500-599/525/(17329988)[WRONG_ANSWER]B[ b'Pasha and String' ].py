e5b1ad51273384a79bf35dffc5153351141ae7a8
Yashar Shahi <yashar_sb_sb@yahoo.com>
2016-04-15 22:03:50 +0430

    submit 17329988 for 525/B/[Pasha and String] with lang: PyPy 3 & verdict: WRONG_ANSWER

---
+++ 500-599/525/(17329988)[WRONG_ANSWER]B[ b'Pasha and String' ].py
@@ -0,0 +1,17 @@
+def s():
+	s = list(input())
+	l = len(s)-1
+	input()
+	a = list(map(int,input().split(' ')))
+	a.sort()
+	a.append(l//2+1)
+	f = False
+	t = 0
+	for i in a:
+		if f:
+			for k in range(t,i-1):
+				s[k], s[l-k] = s[l-k], s[k]
+		f = not f
+		t = i-1
+	print(*s,sep='')
+s()
