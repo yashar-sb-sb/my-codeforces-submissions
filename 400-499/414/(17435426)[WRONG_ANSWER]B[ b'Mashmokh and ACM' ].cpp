919ddbfd17a424afa817f6c24548e56293a0c50d
Yashar Shahi <yashar_sb_sb@yahoo.com>
2016-04-22 18:11:10 +0430

    submit 17435426 for 414/B/[Mashmokh and ACM] with lang: GNU C++11 & verdict: WRONG_ANSWER

---
+++ 400-499/414/(17435426)[WRONG_ANSWER]B[ b'Mashmokh and ACM' ].cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+map<int,int> D[2001];
+
+int mod = 1e9+7;
+
+int f(int n, int k)
+{
+	if(k==1)return 1;
+	else if(D[n].find(k) != D[n].end())return D[n][k];
+	int res = 0;
+	for(int i = 1; i <= n; ++i)
+	{
+		if(!(n%i))res = (res+f(i,k-1))%mod;
+	}
+	D[n][k] = res;
+}
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	int n,k;
+	cin>>n>>k;
+	int res = 0;
+	for(int i = 1; i <= n; ++i)res = (res + f(i,k))%mod;
+	cout<<res;
+	return 0;
+}
+
