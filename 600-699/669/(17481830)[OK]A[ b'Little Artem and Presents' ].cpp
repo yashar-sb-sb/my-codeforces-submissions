0d5c2bc71abe9f1f53874f4071dcd90ddd683a5d
Yashar Shahi <yashar_sb_sb@yahoo.com>
2016-04-24 21:08:25 +0430

    submit 17481830 for 669/A/[Little Artem and Presents] with lang: GNU C++11 & verdict: OK

---
+++ 600-699/669/(17481830)[OK]A[ b'Little Artem and Presents' ].cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	int n;
+	cin>>n;
+	cout<<n/3*2+(n%3>0);
+	return 0;
+}
+
