bdcfab5c1bcfa8854ba43426fad0ed7252af5808
Yashar Shahi <yashar_sb_sb@yahoo.com>
2016-04-29 21:48:12 +0430

    submit 17575803 for 667/A/[Pouring Rain] with lang: GNU C++11 & verdict: WRONG_ANSWER

---
+++ 600-699/667/(17575803)[WRONG_ANSWER]A[ b'Pouring Rain' ].cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+ldb pi = 3.141592653589;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	ldb d,h,v,e;
+	cin>>d>>h>>v>>e;
+	if(4/(pi*v*v)>e+0.000000001)
+	{
+		cout<<"YES\n"<<setprecision(10)<<h/(4/(pi*v*v)-e)<<endl;
+	}
+	else cout<<"NO"<<endl;
+	return 0;
+}
