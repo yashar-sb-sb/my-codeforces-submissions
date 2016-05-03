6c8003e040e242bb5170b32e25e4198642a13089
Yashar Shahi <yashar_sb_sb@yahoo.com>
2016-05-03 18:14:27 +0430

    submit 17699061 for 664/A/[Complicated GCD] with lang: GNU C++11 & verdict: OK

---
+++ 600-699/664/(17699061)[OK]A[ b'Complicated GCD' ].cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	string s,t;
+	cin>>s>>t;
+	if(s==t)cout<<t;
+	else cout<<1;
+	return 0;
+}
