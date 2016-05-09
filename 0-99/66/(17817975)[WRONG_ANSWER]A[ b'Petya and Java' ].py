981e66227f66089d865d8d6fbc94bba39b644a4e
Yashar Shahi <yashar_sb_sb@yahoo.com>
2016-05-09 14:29:12 +0430

    submit 17817975 for 66/A/[Petya and Java] with lang: PyPy 3 & verdict: WRONG_ANSWER

---
+++ 0-99/66/(17817975)[WRONG_ANSWER]A[ b'Petya and Java' ].py
@@ -0,0 +1,13 @@
+def f():
+	a = int(input())
+	if a <= 127 and a >= -128:
+		print('byte')
+	elif a <= 32767 and a >= -32768:
+		print('short')
+	elif a <= 2147483647 and a >= -2147483648:
+		print('in')
+	elif a <= 9223372036854775807 and a >= -9223372036854775808:
+		print('long')
+	else:
+		print('BigInteger')
+f()
