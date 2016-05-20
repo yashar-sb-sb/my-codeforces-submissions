17ab30cb9b2a63af96827eccbf1e4711176aa45d
Yashar Shahi <yashar_sb_sb@yahoo.com>
2016-05-20 12:05:22 +0430

    submit 18001124 for 349/B/[Color the Fence] with lang: GNU C++11 & verdict: WRONG_ANSWER

---
+++ 300-399/349/(18001124)[WRONG_ANSWER]B[ b'Color the Fence' ].cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int A[10];
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	int v,m = 1e5,l;
+	cin>>v;
+	for(int i = 1; i <10; ++i)
+	{
+		cin>>A[i];
+		m = min(A[i],m);
+	}
+	l = v/m;
+	if(!l)cout<<-1;
+	while(l--)
+	{
+		for(int i = 9; i; --i)
+		{
+			if((v-A[i])/m == l)
+			{
+				cout<<i;
+				v -= A[i];
+				break;
+			}
+		}
+	}
+	return 0;
+}
