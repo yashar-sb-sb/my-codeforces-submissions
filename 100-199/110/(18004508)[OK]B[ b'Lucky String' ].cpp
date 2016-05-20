774322dd5f758418472df5d3d477f8e4f566bf66
Yashar Shahi <yashar_sb_sb@yahoo.com>
2016-05-20 16:52:02 +0430

    submit 18004508 for 110/B/[Lucky String] with lang: GNU C++11 & verdict: OK

---
+++ 100-199/110/(18004508)[OK]B[ b'Lucky String' ].cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	int n;
+	cin>>n;
+	for(int i = 0; i < n; ++i)cout<<char('a'+i%4);
+	return 0;
+}
