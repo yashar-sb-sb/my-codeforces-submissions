06642e3c0f052e91e918c60336bf1c6cfe9060dd
Yashar Shahi <yashar_sb_sb@yahoo.com>
2016-05-20 10:31:46 +0430

    submit 18000168 for 322/B/[Ciel and Flowers] with lang: GNU C++11 & verdict: WRONG_ANSWER

---
+++ 300-399/322/(18000168)[WRONG_ANSWER]B[ b'Ciel and Flowers' ].cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+#define mi(a,b,c) min(a, min(b,c))
+#define ma(a,b,c) max(a, max(b,c))
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	int a,b,c;
+	cin>>a>>b>>c;
+	cout<<ma(a/3 + b/3 + c/3 + mi(a%3, b%3, c%3), (a+1)/3 + (b+1)/3 + (c+1)/3 + mi((a+1)%3, (b+1)%3, (c+1)%3) - 1, (a+2)/3 + (b+2)/3 + (c+2)/3 + mi((a+2)%3, (b+2)%3, (c+2)%3) - 2);
+	return 0;
+}
