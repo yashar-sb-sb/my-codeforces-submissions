b3933bd284ae7c34773f76270ebafd98b9e0b185
Yashar Shahi <yashar_sb_sb@yahoo.com>
2016-06-09 14:40:22 +0430

    submit 18339318 for 680/A/[Bear and Five Cards] with lang: GNU C++11 & verdict: OK

---
+++ 600-699/680/(18339318)[OK]A[ b'Bear and Five Cards' ].cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int t[6],s=0;
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	for(int i = 0; i < 5; s += t[i++])cin>>t[i];
+	int m = s;
+	sort(t,t+6);
+	for(int i = 1; i < 5; ++i)
+	{
+		if(t[i] == t[i+1])m = min(s-(2+(t[i-1]==t[i]))*t[i],m);
+	}
+	cout<<m;
+	return 0;
+}
