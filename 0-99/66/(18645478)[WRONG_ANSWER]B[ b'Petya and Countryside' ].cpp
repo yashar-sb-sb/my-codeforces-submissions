e8abd06d45f0d03971a5037c00ea4f0bf18921da
Yashar Shahi <yashar_sb_sb@yahoo.com>
2016-06-22 15:52:38 +0430

    submit 18645478 for 66/B/[Petya and Countryside] with lang: GNU C++ & verdict: WRONG_ANSWER

---
+++ 0-99/66/(18645478)[WRONG_ANSWER]B[ b'Petya and Countryside' ].cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	int n,same = 0,cu = 0,ma= 0,inp,pr=0;
+	bool f = true;
+	cin>>n;
+	while(n--)
+	{
+		cin>>inp;
+		if(f && inp>pr)
+		{
+			ma = max(cu,ma);
+			cu = same+1;
+			same = 1;
+			f = false;
+		}
+		else if(inp == pr)
+		{
+			++same;
+			++cu;
+		}
+		else
+		{
+			++cu;
+			f = true;
+			same = 1;
+		}
+		pr = inp;
+	}
+	cout<<max(cu,ma)<<endl;
+	return 0;
+}
