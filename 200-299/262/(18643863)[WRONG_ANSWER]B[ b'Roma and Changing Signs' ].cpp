426e3c8594caa700b5c3195a89133605ae70dce9
Yashar Shahi <yashar_sb_sb@yahoo.com>
2016-06-22 14:00:12 +0430

    submit 18643863 for 262/B/[Roma and Changing Signs] with lang: GNU C++ & verdict: WRONG_ANSWER

---
+++ 200-299/262/(18643863)[WRONG_ANSWER]B[ b'Roma and Changing Signs' ].cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	int n,k,s=0,f,inp;
+	cin>>n>>k>>inp;
+	while(n--)
+	{
+		cin>>inp;
+		if(inp<=0)
+		{
+			if(k>0)
+			{
+				--k;
+				inp*=-1;
+			}
+		}
+		f = min(f,abs(inp));
+		s+=inp;
+	}
+	if(k&1)s+=-2*f;
+	cout<<s<<endl;
+	return 0;
+}
