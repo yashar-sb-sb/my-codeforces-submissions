3cfdbe67872770a76de6e6ca6994669952c1d482
Yashar Shahi <yashar_sb_sb@yahoo.com>
2016-06-22 11:54:20 +0430

    submit 18641917 for 673/A/[Bear and Game] with lang: GNU C++ & verdict: OK

---
+++ 600-699/673/(18641917)[OK]A[ b'Bear and Game' ].cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	int n,pr=0,cu;
+	cin>>n;
+	while(n--)
+	{
+		cin>>cu;
+		if(cu>pr+15)
+		{
+			cout<<pr+15<<endl;
+			return 0;
+		}
+		pr = cu;
+	}
+	cout<<min(90,cu+15)<<endl;
+	return 0;
+}
