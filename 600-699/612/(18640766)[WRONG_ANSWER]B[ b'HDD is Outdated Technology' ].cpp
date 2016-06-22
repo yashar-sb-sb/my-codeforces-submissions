dfffd3c5ab4161ded8fb065f84ae634b3da9f2fd
Yashar Shahi <yashar_sb_sb@yahoo.com>
2016-06-22 10:10:41 +0430

    submit 18640766 for 612/B/[HDD is Outdated Technology] with lang: GNU C++ & verdict: WRONG_ANSWER

---
+++ 600-699/612/(18640766)[WRONG_ANSWER]B[ b'HDD is Outdated Technology' ].cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int A[int(2e5+1)];
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	int n,ind,res = 0;
+	cin>>n;
+	for(int i =0; i < n; ++i)
+	{
+		cin>>ind;
+		A[ind] = i;
+	}
+	for(int i = 1; i < n; ++i)res += abs(A[i] - A[i+1]);
+	cout<<res<<endl;
+	return 0;
+}
