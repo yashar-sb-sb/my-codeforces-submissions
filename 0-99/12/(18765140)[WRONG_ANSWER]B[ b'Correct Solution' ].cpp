d0128aba51876a46ef826ec568d6d3101710d594
Yashar Shahi <yashar_sb_sb@yahoo.com>
2016-06-28 14:05:53 +0430

    submit 18765140 for 12/B/[Correct Solution?] with lang: GNU C++11 & verdict: WRONG_ANSWER

---
+++ 0-99/12/(18765140)[WRONG_ANSWER]B[ b'Correct Solution' ].cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	string n,m;
+	cin>>n>>m;
+	if(m[0] == '0')return cout<<"WRONG_ANSWER",0;
+	for(int i = 2; i < m.size(); ++i)
+		if(m[i] < m[i-1])return cout<<"WRONG_ANSWER",0;
+	sort(n.begin(),n.end());
+	sort(m.begin(),m.end());
+	if(m!=n)return cout<<"WRONG_ANSWER",0;
+	cout<<"OK";
+	return 0;
+}
