1dbe672304ec06df245204ffc8da94dd0cb77367
Yashar Shahi <yashar_sb_sb@yahoo.com>
2016-06-28 13:28:22 +0430

    submit 18764645 for 332/B/[Maximum Absurdity] with lang: GNU C++11 & verdict: WRONG_ANSWER

---
+++ 300-399/332/(18764645)[WRONG_ANSWER]B[ b'Maximum Absurdity' ].cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int A[int(2+2e5)];
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	int n,k;
+	cin>>n>>k;
+	map<int,int> m;
+	for(int i = 1; i <= n; ++i)cin>>A[i];
+	for(int i = n; i > 0; --i)
+	{
+		A[i]+=A[i+1];
+	}
+	int ma = 0,f,s;
+	for(int i = k+1; i <= n + 1 -k; ++i)
+	{
+		if(!m[A[i-k] - A[i]])m[A[i-k] - A[i]] = i - k;
+		if(A[i] - A[i+k] + m.rbegin()->first > ma)
+		{
+			ma = A[i] - A[i+k] + m.rbegin()->first;
+			f = m.rbegin()->second;
+			s = i;
+		}
+	}
+	cout<<f<<' ' <<s<<endl;
+	return 0;
+}
