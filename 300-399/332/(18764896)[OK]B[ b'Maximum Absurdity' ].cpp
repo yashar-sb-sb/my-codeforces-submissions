292e749d048341ee367465df56760cde1138de7b
Yashar Shahi <yashar_sb_sb@yahoo.com>
2016-06-28 13:48:34 +0430

    submit 18764896 for 332/B/[Maximum Absurdity] with lang: GNU C++11 & verdict: OK

---
+++ 300-399/332/(18764896)[OK]B[ b'Maximum Absurdity' ].cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+LL A[int(2+2e5)];
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	int n,k;
+	cin>>n>>k;
+	LL m = 0;
+	for(int i = 1; i <= n; ++i)cin>>A[i];
+	for(int i = n; i > 0; --i)
+	{
+		A[i]+=A[i+1];
+	}
+	LL ma = 0,f,s,a;
+	for(int i = k+1; i <= n + 1 -k; ++i)
+	{
+		if(A[i-k] - A[i] > m)a = i-k, m = A[i-k] - A[i];
+		if(A[i] - A[i+k] + m > ma)
+		{
+			ma = A[i] - A[i+k] + m;
+			s = i;
+			f = a;
+		}
+	}
+	cout<<f<<' ' <<s<<endl;
+	return 0;
+}
