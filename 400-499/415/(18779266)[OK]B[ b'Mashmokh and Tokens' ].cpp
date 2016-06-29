5fb7f880a2441fb8300dcace1036991983a23777
Yashar Shahi <yashar_sb_sb@yahoo.com>
2016-06-29 12:24:41 +0430

    submit 18779266 for 415/B/[Mashmokh and Tokens] with lang: GNU C++11 & verdict: OK

---
+++ 400-499/415/(18779266)[OK]B[ b'Mashmokh and Tokens' ].cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	LL n,a,b,inp;
+	cin>>n>>a>>b;
+	while(n--)
+	{
+		cin>>inp;
+		cout<<(inp*a%b)/a<<' ';
+	}
+	return 0;
+}
