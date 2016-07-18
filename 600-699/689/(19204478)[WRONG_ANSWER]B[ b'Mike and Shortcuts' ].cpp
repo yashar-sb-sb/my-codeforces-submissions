9dd8e4811de4157f366185d23191cad0817807f0
Yashar Shahi <yashar_sb_sb@yahoo.com>
2016-07-18 11:40:56 +0430

    submit 19204478 for 689/B/[Mike and Shortcuts] with lang: GNU C++11 & verdict: WRONG_ANSWER

---
+++ 600-699/689/(19204478)[WRONG_ANSWER]B[ b'Mike and Shortcuts' ].cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+vector<int> A[int(2e5+1)];
+int R[int(2e5+1)];
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	int n,a;
+	cin>>n;
+	for(int i = 1; i <= n; ++i)
+	{
+		cin>>a;
+		if(a>i)A[a].push_back(i);
+	}
+	for(int i = 2; i <= n; ++i)
+	{
+		R[i] = R[i-1]+1;
+		for(auto j:A[i])
+		{
+			R[i] = min(R[i],R[j]+1);
+		}
+	}
+	for(int i = 1; i <= n; ++i)cout<<R[i]<<' ';
+	return 0;
+}
