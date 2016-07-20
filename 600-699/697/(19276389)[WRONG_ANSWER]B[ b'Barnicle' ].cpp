714021642d2d85f8e908dcb2a9526262b9bb9c9c
Yashar Shahi <yashar_sb_sb@yahoo.com>
2016-07-20 13:45:27 +0430

    submit 19276389 for 697/B/[Barnicle] with lang: GNU C++11 & verdict: WRONG_ANSWER

---
+++ 600-699/697/(19276389)[WRONG_ANSWER]B[ b'Barnicle' ].cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+string inp()
+{
+	char c;
+	string r = "";
+	while((cin>>c)&&isdigit(c))r.push_back(c);
+	return r;
+}
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	string a,d;
+	int b;
+	a = inp();
+	d = inp();
+	cin>>b;
+	for(int i = 0; i < b; ++i)
+	{
+		if(i<d.size())a.push_back(d[i]);
+		else a.push_back('0');
+	}
+	cout<<a;
+	if(b<d.size())
+	{
+		cout<<'.';
+		for(int i = b; i < d.size(); ++i)cout<<d[i];
+	}
+	cout<<endl;
+	return 0;
+}
