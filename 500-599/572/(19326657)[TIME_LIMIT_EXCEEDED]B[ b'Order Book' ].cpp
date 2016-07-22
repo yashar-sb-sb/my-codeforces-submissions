0e431b0ca2a050e94a206835ee14a8888d32bf3d
Yashar Shahi <yashar_sb_sb@yahoo.com>
2016-07-22 19:27:55 +0430

    submit 19326657 for 572/B/[Order Book] with lang: GNU C++11 & verdict: TIME_LIMIT_EXCEEDED

---
+++ 500-599/572/(19326657)[TIME_LIMIT_EXCEEDED]B[ b'Order Book' ].cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	int n,s,p,q;
+	cin>>n>>s;
+	char d;
+	map<int,int> B,S;
+	while(n--)
+	{
+		cin>>d>>p>>q;
+		if(d=='B')B[p]+=q;
+		else S[p]+=q;
+	}
+	auto si = S.rbegin();
+	for(int i = (s<S.size()?S.size()-s:0); i > 0; ++i)++si;
+	for(; si != S.rend(); ++si)cout<<"S "<<si->first<<' '<<si->second<<'\n';
+	for(auto i = B.rbegin(); s-- && i != B.rend(); ++i)cout<<"B "<<i->first<<' '<<i->second<<'\n';
+	return 0;
+}
