af2485284fce93fc5caa6910cfae6d17c1f5f72e
Yashar Shahi <yashar_sb_sb@yahoo.com>
2016-08-04 16:29:51 +0430

    submit 19614538 for 702/B/[Powers of Two] with lang: PyPy 3 & verdict: OK

---
+++ 700-799/702/(19614538)[OK]B[ b'Powers of Two' ].py
@@ -0,0 +1,14 @@
+def s():
+	input()
+	res = 0
+	ss = dict()
+	nums = [1]
+	while nums[-1] < 10**9: nums.append(nums[-1]*2)
+	for i in map(int,input().split()):
+		for j in nums:
+			if j-i in ss:
+				res += ss[j-i]
+		if i in ss:ss[i] += 1
+		else:ss[i] = 1
+	return res
+print(s())
