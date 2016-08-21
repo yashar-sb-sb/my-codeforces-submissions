437f2dce0d5368fb94f913b76b40743f4c82ee36
Yashar Shahi <yashar_sb_sb@yahoo.com>
2016-08-21 21:34:09 +0430

    submit 20031345 for 56/B/[Spoilt Permutation] with lang: PyPy 3 & verdict: OK

---
+++ 0-99/56/(20031345)[OK]B[ b'Spoilt Permutation' ].py
@@ -0,0 +1,19 @@
+def s():
+	input()
+	a = list(map(int,input().split()))
+	b = list(range(1,len(a)+1))
+	l,r = 0,0
+	for i,v in enumerate(a):
+		if v != b[i]:
+			l = i
+			break
+	for i,v in reversed(list(enumerate(a))):
+		if v != b[i]:
+			r = i
+			break
+	a[l:r+1] = reversed(a[l:r+1])
+	if l!=r and a == b:
+		print(l+1,r+1)
+	else:
+		print(0,0)
+s()
