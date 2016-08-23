6b0f8e9df65eace2c975e727b87724473ea4d7b5
Yashar Shahi <yashar_sb_sb@yahoo.com>
2016-08-23 10:27:47 +0430

    submit 20078102 for 522/B/[Photo to Remember] with lang: PyPy 3 & verdict: TIME_LIMIT_EXCEEDED

---
+++ 500-599/522/(20078102)[TIME_LIMIT_EXCEEDED]B[ b'Photo to Remember' ].py
@@ -0,0 +1,18 @@
+def s():
+	n = int(input())
+	a = [tuple(map(int,input().split()))for _ in range(n)]
+	l = [0]*n
+	r = [0]*n
+	cm = 0
+	for i, v in enumerate(a):
+		cm = max(v[1],cm)
+		l[i] = cm
+	cm = 0
+	for i, v in reversed(list(enumerate(a))):
+		cm = max(v[1],cm)
+		r[i] = cm
+	s = sum(i[0] for i in a)
+	r.append(0)
+	l[-1] = 0
+	print(*[(s-v[0])*max(l[i-1],r[i+1]) for i,v in enumerate(a)])
+s()
