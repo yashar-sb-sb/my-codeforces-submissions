7f8a7d0556dfb6d632f8f4b891895267fc4abdb0
Yashar Shahi <yashar_sb_sb@yahoo.com>
2016-08-24 18:56:34 +0430

    submit 20107376 for 31/B/[Sysadmin Bob] with lang: PyPy 3 & verdict: OK

---
+++ 0-99/31/(20107376)[OK]B[ b'Sysadmin Bob' ].py
@@ -0,0 +1,16 @@
+def s():
+	a = input()
+	if a[0] == '@' or a[-1] == '@':
+		return False
+	a = a.split('@')
+	if len(a) == 2:
+		print(*a,sep='@')
+		return True
+	if len(a) == 1 or min(list(len(i) for i in a[1:-1]))<2:
+		return False
+	print(a[0],end='')
+	for i in a[1:-1]:
+		print('@',i[0],',',i[1:],sep='',end='')
+	print('@',a[-1],sep='')
+	return True
+print(''if s() else 'No solution')
