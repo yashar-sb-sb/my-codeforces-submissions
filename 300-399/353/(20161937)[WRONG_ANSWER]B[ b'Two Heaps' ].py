5295c087cbe62b0ddb83cf268b49d1262d3eb133
Yashar Shahi <yashar_sb_sb@yahoo.com>
2016-08-25 23:25:24 +0430

    submit 20161937 for 353/B/[Two Heaps] with lang: PyPy 3 & verdict: WRONG_ANSWER

---
+++ 300-399/353/(20161937)[WRONG_ANSWER]B[ b'Two Heaps' ].py
@@ -0,0 +1,23 @@
+def s():
+	input()
+	l = [[]for _ in range(100)]
+	a = list(map(int,input().split()))
+	for i,v in enumerate(a):
+		l[v].append(i)
+	c = 1
+	fs = [0,0]
+	for i in l:
+		if len(i) == 0:
+			continue
+		en = iter(i)
+		a[next(en)] = 1 + c
+		fs[c] += 1
+		for e in en:
+			a[e] = 2 - c
+		fs[1-c] += 1
+		if len(i) == 1:
+			c = 1 - c
+			fs[c] -= 1
+	print(fs[0]*fs[1])
+	print(*a)
+s()
