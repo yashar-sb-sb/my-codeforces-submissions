819abddb6d7b2adea6a2902de1883ae7ba36ad1a
Yashar Shahi <yashar_sb_sb@yahoo.com>
2016-08-25 12:52:08 +0430

    submit 20148329 for 190/B/[Surrounded] with lang: PyPy 3 & verdict: OK

---
+++ 100-199/190/(20148329)[OK]B[ b'Surrounded' ].py
@@ -0,0 +1,9 @@
+a = list(map(int,input().split()))
+b = list(map(int,input().split()))
+import math
+d = abs(a[0]-b[0])**2+abs(a[1]-b[1])**2
+if d >= (a[2] + b[2])**2:
+	print((math.sqrt(d)-a[2]-b[2])/2)
+	quit()
+d = math.sqrt(d)
+print(max(max(a[2],b[2])-d-min(a[2],b[2]),0)/2)
