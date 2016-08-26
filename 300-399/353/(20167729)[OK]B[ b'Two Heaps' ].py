df06903db0dcab059ac81d40eb15dc8fc8ea5adc
Yashar Shahi <yashar_sb_sb@yahoo.com>
2016-08-26 08:52:42 +0430

    submit 20167729 for 353/B/[Two Heaps] with lang: PyPy 3 & verdict: OK

---
+++ 300-399/353/(20167729)[OK]B[ b'Two Heaps' ].py
@@ -0,0 +1,28 @@
+def s():
+	input()
+	l = [[]for _ in range(100)]
+	a = list(map(int,input().split()))
+	for i,v in enumerate(a):
+		l[v].append(i)
+	c = 0
+	cc = 0
+	fs = [0,0]
+	for i in l:
+		if len(i) == 0:
+			continue
+		if len(i) == 1:
+			a[i[0]] = c+1
+			fs[c]+=1
+			c = 1 - c
+			continue
+		fs[c] += 1
+		fs[c-1] += 1
+		for e in i[:len(i)//2]:
+			a[e] = 1 + cc
+		for e in i[len(i)//2:]:
+			a[e] = 2 - cc
+		if len(i) % 2 == 1:
+			cc = 1 - cc
+	print(fs[0]*fs[1])
+	print(*a)
+s()
