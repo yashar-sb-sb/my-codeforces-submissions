0e31e7e123daa23922a96ef6c08a008035ef290a
Yashar Shahi <yashar_sb_sb@yahoo.com>
2016-08-30 22:35:14 +0430

    submit 20286868 for 287/B/[Pipeline] with lang: PyPy 3 & verdict: WRONG_ANSWER

---
+++ 200-299/287/(20286868)[WRONG_ANSWER]B[ b'Pipeline' ].py
@@ -0,0 +1,12 @@
+def s():
+    [n,k] = list(map(int,input().split()))
+    n-=1
+    k-=1
+    if k*(k+1)//2 < n:
+        print(-1)
+        return
+    l = k*(k+1)//2 - n
+    import math
+    a = int((-1+math.sqrt(1+8*l))/2)
+    print(k-a)
+s()
