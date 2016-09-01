b8a0286c2797282db2fb9563ca1459c3046f2691
Yashar Shahi <yashar_sb_sb@yahoo.com>
2016-09-01 15:01:56 +0430

    submit 20319026 for 639/B/[Bear and Forgotten Tree 3] with lang: GNU C++ & verdict: OK

---
+++ 600-699/639/(20319026)[OK]B[ b'Bear and Forgotten Tree 3' ].cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+map<pii,int> ma;
+
+int A[int(3e5)];
+LL L[int(3e5)],R[int(3e5)];
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	int n,d,h;
+	cin>>n>>d>>h;
+	if(d>h*2 || n<d+1 || (d==1 && n>2))return cout<<-1,0;
+	for(int i = 0; i < h; ++i)cout<<i+1<<' '<<i+2<<'\n';
+	int next = h+2, prev = 1;
+	for(int i = 0; i < d-h; ++i)
+	{
+		cout<<prev<<' '<<next<<'\n';
+		prev = next++;
+	}
+	if(h==d)prev=2;
+	else prev=1;
+	for(; next <= n; next++)cout<<prev<<' '<<next<<'\n';
+}
