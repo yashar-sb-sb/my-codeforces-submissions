4eb46e89f03a11737f9a6d9fc3402cbb321ae721
Yashar Shahi <yashar_sb_sb@yahoo.com>
2016-11-14 09:39:38 +0330

    submit 22203038 for 722/A/[Broken Clock] with lang: GNU C++11 & verdict: WRONG_ANSWER

---
+++ 700-799/722/(22203038)[WRONG_ANSWER]A[ b'Broken Clock' ].cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<LL,LL> pii;
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	string s;
+	int f;
+	cin>>f>>s;
+	if(s.substr(3)>"59")s[3] = '0';
+	if(f == 24)
+	{
+		if(s.substr(0,2)>"23")s[0] = '0';
+	}
+	else
+	{
+		if(s.substr(0,2) == "00")s[1] = '1';
+		if(s.substr(0,2) > "12")
+		{
+			if(s[1]=='0')s[0]='1';
+			else s[1] = '0';
+		}
+	}
+	cout<<s<<endl;
+	return 0;
+}
