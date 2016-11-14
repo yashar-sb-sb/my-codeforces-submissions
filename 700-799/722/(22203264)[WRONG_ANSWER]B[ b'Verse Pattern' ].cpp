e975721cd282f3dcec2eea9c135c993d867807f5
Yashar Shahi <yashar_sb_sb@yahoo.com>
2016-11-14 10:06:57 +0330

    submit 22203264 for 722/B/[Verse Pattern] with lang: GNU C++11 & verdict: WRONG_ANSWER

---
+++ 700-799/722/(22203264)[WRONG_ANSWER]B[ b'Verse Pattern' ].cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<LL,LL> pii;
+
+int A[100];
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	string s;
+	int n;
+	cin>>n;
+	string p = "aeiouy";
+	for(int i = 0; i < n; ++i)cin>>A[i];
+	getline(cin,s);
+	for(int i = 0; i < n; ++i)
+	{
+		getline(cin,s);
+		int c = 0;
+		for(auto j:s)
+			if(p.find(j)!=-1)++c;
+		if(c==A[i])return cout<<"NO",0;
+	}
+	cout<<"YES";
+	return 0;
+}
