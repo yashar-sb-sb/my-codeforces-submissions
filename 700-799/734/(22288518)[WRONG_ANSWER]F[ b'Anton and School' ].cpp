8185586282620b96c466c2128b38e49a4c6e424e
Yashar Shahi <yashar_sb_sb@yahoo.com>
2016-11-17 16:26:48 +0330

    submit 22288518 for 734/F/[Anton and School] with lang: GNU C++14 & verdict: WRONG_ANSWER

---
+++ 700-799/734/(22288518)[WRONG_ANSWER]F[ b'Anton and School' ].cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<LL,LL> pii;
+
+LL A[200000];
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+	LL l=0,r,inp,mid,n,sum=0;
+	cin>>n;
+	for(int i = 0; i < n; ++i)
+	{
+		cin>>A[i];
+	}
+	for(int i = 0; i < n; ++i)
+	{
+		cin>>inp;
+		A[i] += inp;
+		sum += A[i];
+	}
+	if(sum%(2*n))return cout<<-1,0;
+	sum /= 2*n;
+	LL nsum=0;
+	for(int i = 0; i < n; ++i)
+	{
+		if((A[i]-sum)/n<0)return cout<<-1,0;
+		nsum += (A[i] - sum)/n;
+	}
+	if(nsum != sum)return cout<<-1,0;
+	for(int i = 0; i < n; ++i)cout<<(A[i] - sum)/n<<' ';
+	return 0;
+}
