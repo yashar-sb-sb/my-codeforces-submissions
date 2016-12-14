4a6050cdc725871c131c916875a44a805131bbda
Yashar Shahi <yashar_sb_sb@yahoo.com>
2016-12-14 18:36:29 +0330

    submit 22959700 for 743/C/[Vladik and fractions] with lang: GNU C++14 & verdict: CHALLENGED

---
+++ 700-799/743/(22959700)[CHALLENGED]C[ b'Vladik and fractions' ].cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+
+int main()
+{
+	int n;
+	cin>>n;
+	cout<<n<<' '<<n+1<<' '<<n*n+n<<endl;
+	return 0;
+}
