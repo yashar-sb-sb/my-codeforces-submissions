0916d308acd3d6a97b877d8d9652e8bb0ae50eb7
Yashar Shahi <yashar_sb_sb@yahoo.com>
2016-12-14 18:26:32 +0330

    submit 22956933 for 743/B/[Chloe and the sequence ] with lang: GNU C++14 & verdict: OK

---
+++ 700-799/743/(22956933)[OK]B[ b'Chloe and the sequence ' ].cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+LL solve(LL n, LL k)
+{
+	if(k == (1LL<<n-1LL))return n;
+	if(k < (1LL<<n-1LL))return solve(n-1LL,k);
+	return solve(n-1LL,k-(1LL<<n-1LL));
+}
+
+int main()
+{
+	LL n,k;
+	cin>>n>>k;
+	cout<<solve(n,k)<<endl;
+	return 0;
+}
