4834ea74e288da8533cc75da3caca1b560847f1d
Yashar Shahi <yashar_sb_sb@yahoo.com>
2016-12-20 19:58:30 +0330

    submit 23180931 for 746/C/[Tram] with lang: GNU C++14 & verdict: WRONG_ANSWER

---
+++ 700-799/746/(23180931)[WRONG_ANSWER]C[ b'Tram' ].cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int p,d,s;
+int f(int x)
+{
+	if((x-p)*d>=0)
+	{
+		int t = abs(x-p);
+		p = x;
+		return t;
+	}
+	if(d<0)
+		return p + f((d=1,p=0,x));
+	else
+		return s-p + f((d=-1,p=s,x));
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	int x1,x2,t1,t2;
+	cin>>s>>x1>>x2>>t1>>t2>>p>>d;
+	int ttime = 0, itime = abs(x1-x2)*t2;
+	ttime += f(x1)*t1 + f(x2)*t1;
+	cout<<min(ttime,itime)<<endl;
+	return 0;
+}
