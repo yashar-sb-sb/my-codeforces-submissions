d52aa7e9a5593b846ccbd0889e9bf0926c11fd18
Yashar Shahi <yashar_sb_sb@yahoo.com>
2016-12-21 10:23:57 +0330

    submit 23192615 for 747/B/[Mammoth's Genome Decoding] with lang: GNU C++14 & verdict: RUNTIME_ERROR

---
+++ 700-799/747/(23192615)[RUNTIME_ERROR]B[ bMammoth's Genome Decoding ].cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+int A[1000];
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	int n;
+	cin>>n;
+	if(n%4)return cout<<"===",0;
+	string s,pat = "ACGT";
+	cin>>s;
+	for(auto i:s)++A[i];
+	int ind = 0;
+	for(int i = 'A'; i < 'Z'; ++i)
+	{
+		if(A[i]>n/4)return cout<<"===",0;
+		if(pat.find(i)!=-1)for(int j = A[i]; j < n/4; ++j)
+		{
+			while(s[ind]!='?')++ind;
+			s[ind] = i;
+		}
+	}
+	cout<<s<<endl;
+	return 0;
+}
