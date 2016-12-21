82dad210f36f82746afbd3e0b2ec0051d86fd8d7
Yashar Shahi <yashar_sb_sb@yahoo.com>
2016-12-21 11:17:53 +0330

    submit 23193466 for 747/E/[Comments] with lang: GNU C++14 & verdict: OK

---
+++ 700-799/747/(23193466)[OK]E[ b'Comments' ].cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+char S[1000009];
+vector<string> ans[1000001];
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	string s;
+	stack<int> st;
+	int p,ind=0,sz=0;
+	char c;
+	cin>>S;
+	while(S[ind])
+	{
+		s="";
+		while(S[ind]!=',')s.push_back(S[ind++]);
+		++ind;
+		p=0;
+		while(S[ind]&&S[ind]!=',')p = p*10+S[ind++]-'0';
+		ans[st.size()].push_back(s);
+		st.push(p);
+		sz = max(sz,int(st.size()));
+		while(!st.empty() && st.top()==0)
+		{
+			st.pop();
+			if(!st.empty())st.top()-=1;
+		}
+		++ind;
+	}
+	cout<<sz<<'\n';
+	for(int i = 0; i < sz; ++i)
+	{
+		for(auto j:ans[i])cout<<j<<' ';cout<<'\n';
+	}
+	return 0;
+}
