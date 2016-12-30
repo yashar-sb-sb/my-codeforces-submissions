59f8d0bbcc66b267e19175792628229385bc6089
Yashar Shahi <yashar_sb_sb@yahoo.com>
2016-12-30 20:06:13 +0330

    submit 23451543 for 750/D/[New Year and Fireworks] with lang: GNU C++14 & verdict: OK

---
+++ 700-799/750/(23451543)[OK]D[ b'New Year and Fireworks' ].cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+struct tok
+{
+	int x, y, dx, dy;
+	tok(int X, int Y, int DX, int DY) :x(X), y(Y), dx(DX), dy(DY)
+	{}
+};
+
+int D[400][400][151];
+int times[151];
+int ANS[400][400];
+int ans = 0, sz = -1;
+
+void go(int x, int y, int dx, int dy, int ti)
+{
+	int xx = 1+dx, yy = 1+dy;
+	if(ti>=sz||(D[x][y][ti]&(1<<(xx*10+yy))))return;
+	D[x][y][ti]|=(1<<(xx*10+yy));
+	ans += !ANS[x][y];
+	ANS[x][y] = 1;
+	if(times[ti])
+	{
+			if(dx && dy)
+			{
+				go(x+dx, y, dx, 0,ti+1);
+				go(x, y+dy, 0, dy,ti+1);
+			}
+			else if(dx)
+			{
+				go(x+dx, y-1, dx, -1,ti+1);
+				go(x+dx, y+1, dx, 1,ti+1);
+			}
+			else if(dy)
+			{
+				go(x-1, y+dy, -1, dy,ti+1);
+				go(x+1, y+dy, 1, dy,ti+1);
+			}
+	}
+	else go(x + dx, y + dy, dx, dy, ti + 1);
+	return;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	int n,t;
+	cin>>n;
+	for(int i = 0; i < n; ++i)
+	{
+		cin>>t;
+		for(int j = 0; j < t; ++j)++sz;
+		times[sz] = 1;
+	}
+	++sz;
+	go(200,200,1,0,0);
+	cout<<ans<<endl;
+	return 0;
+}
