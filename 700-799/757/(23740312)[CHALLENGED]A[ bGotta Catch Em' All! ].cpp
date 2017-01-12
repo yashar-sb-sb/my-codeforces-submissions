935afa54d8bb104d63f6f894ad8adffce9b311ca
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-01-12 19:09:53 +0330

    submit 23740312 for 757/A/[Gotta Catch Em' All!] with lang: GNU C++14 & verdict: CHALLENGED

---
+++ 700-799/757/(23740312)[CHALLENGED]A[ bGotta Catch Em' All! ].cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int A['z'+1];
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	char c;
+	while(cin>>c)++A[c];
+	int ans = 1000000;
+	ans = min(ans,A['B']);
+	ans = min(ans,A['u']/2);
+	ans = min(ans,A['l']);
+	ans = min(ans,A['b']/2);
+	ans = min(ans,A['a']/2);
+	ans = min(ans,A['s']);
+	ans = min(ans,A['r']);
+	cout<<ans<<endl;
+	return 0;
+}
