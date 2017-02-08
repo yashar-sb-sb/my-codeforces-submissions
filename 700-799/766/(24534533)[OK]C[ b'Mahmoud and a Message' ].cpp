9ca1a91b1a6fd7a868a069aeea21b05ddeab32e3
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-02-08 22:21:27 +0330

    submit 24534533 for 766/C/[Mahmoud and a Message] with lang: GNU C++14 & verdict: OK

---
+++ 700-799/766/(24534533)[OK]C[ b'Mahmoud and a Message' ].cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int A[1000], B[26];
+int mod = 1e9+7;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    int n, ma = 1, mi = 1, mil;
+    cin>>n;
+    string s;
+    cin>>s;
+    for(int i = 0; i < 26; ++i)cin>>B[i];
+    for(int i = 0; i < n; ++i)
+    {
+    	mil = 1000;
+    	for(int j = i; j > -1; --j)
+    	{
+    		mil = min(mil,B[s[j]-'a']);
+    		if(mil<i-j+1)break;
+    		A[i] = (A[i] + (j<1?1:A[j-1]))%mod;
+    		ma = max(ma, i-j+1);
+    	}
+    }
+    mil = 1000;
+    int sz = 0;
+    for(int i = 0; i < n; ++i)
+    {
+    	mil = min(mil, B[s[i]-'a']);
+    	++sz;
+    	if(mil < sz)
+    	{
+    		sz = 1;
+    		mil = B[s[i]-'a'];
+    		++mi;
+    	}
+    }
+    cout<<A[n-1]<<'\n'<<ma<<'\n'<<mi<<'\n';
+	return 0;
+}
