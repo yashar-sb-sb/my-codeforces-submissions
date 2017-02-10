faf656b299c5aceacdfdf5ef7051d49a38fd475b
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-02-10 17:01:34 +0330

    submit 24570420 for 761/A/[Dasha and Stairs] with lang: GNU C++14 & verdict: WRONG_ANSWER

---
+++ 700-799/761/(24570420)[WRONG_ANSWER]A[ b'Dasha and Stairs' ].cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    int a, b;
+    cin>>a>>b;
+    if(a+1==b || a == b)cout<<"YES";
+    else cout<<"NO";
+	return 0;
+}
