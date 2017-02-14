3ca53e6975a3ffef80ea09750caca7a8a90a6b8d
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-02-14 11:38:43 +0330

    submit 24644726 for 765/A/[Neverending competitions] with lang: GNU C++14 & verdict: OK

---
+++ 700-799/765/(24644726)[OK]A[ b'Neverending competitions' ].cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    int n;
+    cin>>n;
+    cout<<(n&1?"contest":"home");
+   	return 0;
+}
