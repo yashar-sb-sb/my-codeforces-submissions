be5d08924fae1bc670e35cfaf7e8b0197224a4db
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-02-23 18:32:27 +0330

    submit 24922912 for 776/B/[Sherlock and his girlfriend] with lang: GNU C++14 & verdict: OK

---
+++ 700-799/776/(24922912)[OK]B[ b'Sherlock and his girlfriend' ].cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int S[100002];
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    int n;
+    cin>>n;
+    ++n;
+    for(int i = 2; i <= n; ++i)
+    {
+    	if(!S[i])S[i] = 1;
+    	else continue;
+    	if(i < 10000)
+    	for(int j = i*i; j <= n; j += i)S[j] = 2;
+    }
+    if(n<4)cout<<"1\n";
+    else cout<<"2\n";
+    for(int i = 2; i <= n; ++i)
+    	cout<<S[i]<<' ';
+   	return 0;
+}
