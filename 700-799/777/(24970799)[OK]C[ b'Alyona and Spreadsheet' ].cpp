78f263d41181b671f165be87430c11243002d13c
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-02-24 14:37:08 +0330

    submit 24970799 for 777/C/[Alyona and Spreadsheet] with lang: GNU C++14 & verdict: OK

---
+++ 700-799/777/(24970799)[OK]C[ b'Alyona and Spreadsheet' ].cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    int n,m;
+    cin>>n>>m;
+    vector<vector<int>> A(n,vector<int>(m,0));
+    vector<vector<int>> S(n,vector<int>(m,1));
+    vector<int> ma(n,0);
+    for(int i = 0; i < n; ++i)
+    	for(int j = 0; j < m; ++j)
+    		cin>>A[i][j];
+    for(int i = 1; i < n; ++i)
+    	for(int j = 0; j < m; ++j)
+    		if(A[i][j] >= A[i-1][j])
+    			S[i][j] = S[i-1][j] + 1;
+    for(int i = 0; i < n; ++i)
+    	for(int j = 0; j < m; ++j)
+    		ma[i] = max(S[i][j],ma[i]);
+    int k;
+    cin>>k;
+    int l,r;
+    while(k--)
+    {
+    	cin>>l>>r;
+    	l = r -l+1;
+    	--r;
+    	if(ma[r] >= l)cout<<"Yes";
+    	else cout<<"No";
+    	cout<<'\n';
+    }
+}
