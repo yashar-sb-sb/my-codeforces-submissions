d62e0dea85bfec00ca73166fc0b3525c87dfbaa8
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-03-19 11:01:52 +0330

    submit 25638906 for 791/B/[Bear and Friendship Condition] with lang: GNU C++14 & verdict: OK

---
+++ 700-799/791/(25638906)[OK]B[ b'Bear and Friendship Condition' ].cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+vector<int> G[200000];
+int vis[200000];
+
+LL f(int v)
+{
+    if(vis[v])return 0;
+    LL res = vis[v] = 1;
+    for(int i:G[v])
+        res += f(i);
+    return res;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    LL n,m,a,b;
+    cin>>n>>m;
+    for(int i = 0; i < m; ++i)
+    {
+        cin>>a>>b;
+        G[a].push_back(b);
+        G[b].push_back(a);
+    }
+    for(int i = 1; i <= n; ++i)
+    {
+        LL cur = f(i);
+        m -= cur*(cur-1)/2;
+    }
+    cout<<(m?"NO":"YES")<<endl;
+    return 0;
+}
