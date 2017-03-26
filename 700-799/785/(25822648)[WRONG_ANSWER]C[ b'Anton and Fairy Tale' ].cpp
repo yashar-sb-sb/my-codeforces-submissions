64fc6fff8a6c7ff4c4303c8585628ff55dc19d8d
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-03-26 20:03:52 +0430

    submit 25822648 for 785/C/[Anton and Fairy Tale] with lang: GNU C++14 & verdict: WRONG_ANSWER

---
+++ 700-799/785/(25822648)[WRONG_ANSWER]C[ b'Anton and Fairy Tale' ].cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+LL n,m;
+
+bool f(LL day)
+{
+    if(day >= n)
+        return true;
+    if(day <= m)
+        return false;
+    day = day - m;
+    return day*(day+1)/2 +m >= n;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin>>n>>m;
+    if(m>n)
+    {
+        cout<<n<<endl;
+        return 0;
+    }
+
+    LL l, r;
+    r = 2e18;
+    l = 0;
+
+    while(r - l > 1)
+    {
+        LL m = (l + r) / 2;
+        if(f(m))
+            r = m;
+        else
+            l = m;
+    }
+    cout<<r<<endl;
+
+    return 0;
+}
