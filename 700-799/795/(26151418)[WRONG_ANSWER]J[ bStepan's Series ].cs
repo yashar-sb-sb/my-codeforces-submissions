841b4a1a2f5c13b7cd9471ef52c71fec4e100fc1
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-04-05 21:08:11 +0430

    submit 26151418 for 795/J/[Stepan's Series] with lang: Mono C# & verdict: WRONG_ANSWER

---
+++ 700-799/795/(26151418)[WRONG_ANSWER]J[ bStepan's Series ].cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n,k;
+            string s;
+            var t = Console.ReadLine().Split(' ');
+            n = int.Parse(t[0]);
+            k = int.Parse(t[1]);
+            s = Console.ReadLine();
+            int m = 0, d = 0;
+            foreach (var i in s)
+            {
+                if(i == 'N')
+                    ++d;
+                else d = 0;
+                if(d>m) m = d;
+            }
+            Console.WriteLine(m == k ? "YES" : "NO");
+        }
+    }
+}
