68b09ee539464d1de2ad7c1f1f645bcf208f0803
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-04-05 20:53:00 +0430

    submit 26150178 for 795/D/[Lie or Truth] with lang: Java 8 & verdict: WRONG_ANSWER

---
+++ 700-799/795/(26150178)[WRONG_ANSWER]D[ b'Lie or Truth' ].java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        Scanner sc= new Scanner(System.in);
+        int n,l,r;
+        n = sc.nextInt();
+        l = sc.nextInt();
+        r = sc.nextInt();
+        int[] a, b, c;
+        a = new int[n];
+        b = new int[n];
+        c = new int[n+1];
+        for (int i = 0; i < n+1; i++)
+            c[i] = 0;
+        for(int i = 0; i < n; ++i)
+            a[i] = sc.nextInt();
+        for(int i = 0; i < n; ++i)
+            b[i] = sc.nextInt();
+        for(int i = l-1; i < r; ++i)
+        {
+            ++c[a[i]];
+            --c[b[i]];
+        }
+        for(int i = 0; i <= n; ++i)
+            if(c[i] != 0)
+            {
+                System.out.println("LIE");
+                return;
+            }
+        System.out.println("TRUTH");
+    }
+
+}
