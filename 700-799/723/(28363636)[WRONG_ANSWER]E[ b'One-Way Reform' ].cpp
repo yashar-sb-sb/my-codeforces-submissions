b67d4a9dcadae3beb783b15e3c033d24a75209d4
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-07-08 12:23:16 +0430

    submit 28363636 for 723/E/[One-Way Reform] with lang: GNU C++14 & verdict: WRONG_ANSWER

---
+++ 700-799/723/(28363636)[WRONG_ANSWER]E[ b'One-Way Reform' ].cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int A[201];
+pii M[20000];
+
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, m;
+        cin>>n>>m;
+        memset(A, 0, sizeof(A));
+        for(int i = 0; i < m; ++i)
+        {
+            cin>>M[i].first>>M[i].second;
+            ++A[M[i].first];
+            --A[M[i].second];
+        }
+        bool flag;
+        do
+        {
+            flag = false;
+            for(int i = 0; i < m; ++i)
+            {
+                if(A[M[i].first]&&A[M[i].second])
+                {
+                    if(A[M[i].first] == 2 || A[M[i].second] == -2)
+                    {
+                        A[M[i].first] -= 2;
+                        A[M[i].second] += 2;
+                        swap(M[i].first, M[i].second);
+                        flag = true;
+                    }
+                }
+            }
+        }
+        while(flag);
+        int count = 0;
+        for(int i = 1; i<= n; ++i)
+            count += !A[i];
+        cout<<count<<'\n';
+        for(int i = 0; i < m; ++i)
+            cout<<M[i].first<<' '<<M[i].second<<'\n';
+    }
+    return 0;
+}
