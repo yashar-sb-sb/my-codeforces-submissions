5fd23a8be194bf27cddd9ae87816943462ec2d74
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-07-11 08:29:57 +0430

    submit 28419277 for 716/B/[Complete the Word] with lang: GNU C++14 & verdict: WRONG_ANSWER

---
+++ 700-799/716/(28419277)[WRONG_ANSWER]B[ b'Complete the Word' ].cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int A[26];
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    string s;
+    cin>>s;
+    int n = s.size();
+    for(int i = 0; i < n-25; ++i)
+    {
+        memset(A, 0, sizeof(A));
+        bool can = true;
+        for(int j = 0; j < 26; ++j)
+        {
+            if(s[i+j] != '?')
+            {
+                if(!A[s[i+j] - 'A'])
+                    A[s[i+j] - 'A'] = 1;
+                else
+                {
+                    can = false;
+                    break;
+                }
+            }
+        }
+        if(can)
+        {
+            for(int j = 0; j < 26; ++j)
+            {
+                if(!A[j])
+                {
+                    for(int k = 0; k < 26; ++k)
+                        if(s[i+k] == '?')
+                        {
+                            s[i+k] = j + 'A';
+                            A[j] = 1;
+                            break;
+                        }
+                }
+            }
+            for(int j = 0; j < 26; ++j)
+                cout<<s[i+j];
+            cout<<endl;
+            return 0;
+        }
+    }
+    cout<<-1<<endl;
+    return 0;
+}
