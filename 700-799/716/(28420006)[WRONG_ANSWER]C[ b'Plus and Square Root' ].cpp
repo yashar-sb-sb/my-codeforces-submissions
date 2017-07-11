3426d4f37a7e47f69e2c147394d009b3ccf88577
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-07-11 09:28:32 +0430

    submit 28420006 for 716/C/[Plus and Square Root] with lang: GNU C++14 & verdict: WRONG_ANSWER

---
+++ 700-799/716/(28420006)[WRONG_ANSWER]C[ b'Plus and Square Root' ].cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+vector<LL> ans;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    LL n;
+    cin>>n;
+    LL num = 1e9;
+    while(n>1)
+    {
+        num *= num;
+        ans.push_back((num - LL(1e9))/n);
+        num -= (num - 1e9)/n*n;
+        n--;
+    }
+    ans.push_back(num*num-2);
+    for(int i = ans.size()-1; i >= 0; --i)
+        cout<<ans[i]<<'\n';
+    return 0;
+}
