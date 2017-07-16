cd379c39d55f5b87c87fe0ad20ff132e00617e02
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-07-16 21:05:20 +0430

    submit 28611679 for 825/E/[Minimal Labels] with lang: GNU C++14 & verdict: WRONG_ANSWER

---
+++ 800-899/825/(28611679)[WRONG_ANSWER]E[ b'Minimal Labels' ].cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+vector<int> T[100001];
+int nums[100001];
+int nex = 1;
+set<int> se;
+
+void f(int v)
+{
+    if(nums[v])
+        return;
+    sort(T[v].begin(), T[v].end());
+    for(int i:T[v])
+        f(i);
+    nums[v] = nex++;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int n, m;
+    cin>>n>>m;
+    for(int i = 0; i < m; ++i)
+    {
+        int a, b;
+        cin>>a>>b;
+        T[b].push_back(a);
+    }
+    for(int i = 1; i <= n; ++i)
+        f(i);
+    for(int i = 1; i <= n; ++i)
+        cout<<nums[i]<<' ';
+    cout<<endl;
+    return 0;
+}
