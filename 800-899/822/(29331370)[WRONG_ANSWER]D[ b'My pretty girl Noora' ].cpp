9347efcc416288629f06a491c9e51b253d828eda
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-08-10 20:15:47 +0430

    submit 29331370 for 822/D/[My pretty girl Noora] with lang: GNU C++14 & verdict: WRONG_ANSWER

---
+++ 800-899/822/(29331370)[WRONG_ANSWER]D[ b'My pretty girl Noora' ].cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+const int mod = 1e9 + 7;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    LL t, l, r;
+    cin>>t>>l>>r;
+    LL d = 1;
+    LL sum = 0;
+    for(int i = l; i <= r; ++i)
+    {
+        LL ans = 0;
+        LL cur = i;
+        while(cur > 1)
+        {
+            ans += cur/2 + cur%2*2;
+            cur /= 2;
+        }
+        sum = (sum + ans*d%mod) % mod;
+        d = d * t % mod;
+    }
+    cout<<sum<<endl;
+    return 0;
+}
