24109b560b1008fd8d051af236b1b0a121bde2d4
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-08-18 11:47:37 +0430

    submit 29545375 for 770/C/[Online Courses In BSU] with lang: GNU C++14 & verdict: OK

---
+++ 700-799/770/(29545375)[OK]C[ b'Online Courses In BSU' ].cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+vector<int> G[100001];
+int A[100001];
+int col[100001];
+vector<int> ans;
+
+
+bool f(int v)
+{
+    if(col[v] == 2)
+        return true;
+    if(col[v] == 1)
+        return false;
+    col[v] = 1;
+    for(int i :G[v])
+        if(!f(i))
+            return false;
+    col[v] = 2;
+    ans.push_back(v);
+    return true;
+}
+
+
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int n, k;
+    cin>>n>>k;
+    for(int i = 0; i < k; ++i)
+        cin>>A[i];
+    for(int i = 1; i <= n; ++i)
+    {
+        int t, inp;
+        cin>>t;
+        for(int j = 0; j < t; ++j)
+        {
+            cin>>inp;
+            G[i].push_back(inp);
+        }
+    }
+    for(int i = 0; i < k; ++i)
+        if(!f(A[i]))
+            return cout<<"-1\n", 0;
+    cout<<ans.size()<<'\n';
+    for(int i:ans)
+        cout<<i<<' ';
+    cout<<endl;
+    return 0;
+}
