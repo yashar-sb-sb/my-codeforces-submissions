10d88967b47eb8f02b2fa8b1ae4d3d8f906393b6
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-08-18 14:58:50 +0430

    submit 29551338 for 828/B/[Black Square] with lang: GNU C++14 & verdict: WRONG_ANSWER

---
+++ 800-899/828/(29551338)[WRONG_ANSWER]B[ b'Black Square' ].cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int n, m;
+    int miX = 100, miY = 100, maX = 0, maY = 0, c = 0;
+    char ch;
+    cin>>n>>m;
+    for(int i = 0; i < n; ++i)
+    {
+        for(int j = 0; j < m; ++j)
+        {
+            cin>>ch;
+            if(ch == 'B')
+            {
+                miX = min(i, miX);
+                miY = min(i, miY);
+                maX = max(i, maX);
+                maY = max(i, maY);
+                ++c;
+            }
+        }
+    }
+    if(c == 0)
+        return cout<<"1\n", 0;
+    maX = maX - miX + 1;
+    maY = maY - miY + 1;
+    int ma = max(maX, maY);
+    if(ma > n || ma > m)
+        return cout<<"-1\n", 0;
+    cout<<ma*ma - c<<endl;
+    return 0;
+}
