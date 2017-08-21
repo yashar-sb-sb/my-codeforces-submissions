27895e32c22b07bd52e756ed7fd7f42de87e09e7
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-08-21 22:37:19 +0430

    submit 29665242 for 645/D/[Robot Rapping Results Report] with lang: GNU C++14 & verdict: OK

---
+++ 600-699/645/(29665242)[OK]D[ b'Robot Rapping Results Report' ].cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+vector<int> G[100001];
+pii raps[100001];
+int D[100001];
+
+int ma = 0;
+int n, m;
+vector<int> ans;
+
+void topsort(int v)
+{
+    if(D[v] == 0)
+    {
+        D[v] = -1;
+        ans.push_back(v);
+        for(int t:G[v])
+        {
+            --D[t];
+            topsort(t);
+        }
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    cin>>n>>m;
+    for(int i = 0; i < m; ++i)
+    {
+        cin>>raps[i].first>>raps[i].second;
+        G[raps[i].first].push_back(raps[i].second);
+        ++D[raps[i].second];
+    }
+    for(int i = 1; i <= n; ++i)
+        if(D[i] == 0)
+            topsort(i);
+    set<pii> se;
+    for(int i = 0; i < n-1; ++i)
+        se.insert(pii(ans[i], ans[i+1]));
+
+    for(int i = 0; i < m; ++i)
+    {
+        se.erase(raps[i]);
+        if(se.empty())
+        {
+            cout<<i+1<<endl;
+            return 0;
+        }
+    }
+    cout<<-1<<endl, 0;
+
+    return 0;
+}
