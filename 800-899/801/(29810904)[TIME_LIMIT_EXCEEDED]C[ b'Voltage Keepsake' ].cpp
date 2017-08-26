873a479ffdab44cad836a19da878052ca57cb536
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-08-26 21:08:09 +0430

    submit 29810904 for 801/C/[Voltage Keepsake] with lang: GNU C++14 & verdict: TIME_LIMIT_EXCEEDED

---
+++ 800-899/801/(29810904)[TIME_LIMIT_EXCEEDED]C[ b'Voltage Keepsake' ].cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+vector<pii> A;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int n, p, a, b;
+    cin>>n>>p;
+    A.reserve(n);
+    LL sum = 0;
+    for(int i = 0; i < n; ++i)
+    {
+        cin>>a>>b;
+        A.emplace_back(a,b);
+        sum += a;
+    }
+    if(sum<=p)
+        return cout<<-1<<endl, 0;
+    ldb l = 0, r = numeric_limits<ldb>::max(), eps = 1e-6, m, S;
+    while(r-l > eps)
+    {
+        m = (l+r)/ldb(2);
+        S = 0;
+        for(pii I: A)
+            S += max(ldb(0), m*I.first - I.second);
+        if(S<p*m)
+            l = m;
+        else
+            r = m;
+    }
+    m = (l+r)/ldb(2);
+    cout.precision(10);
+    cout.setf(ios::fixed);
+    cout<<m<<endl;
+    return 0;
+}
