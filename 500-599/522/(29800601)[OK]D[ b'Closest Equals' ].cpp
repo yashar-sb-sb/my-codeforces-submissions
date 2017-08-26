28f659fd6679aca2c76da84489a1ec2b1fc37429
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-08-26 12:33:05 +0430

    submit 29800601 for 522/D/[Closest Equals] with lang: GNU C++14 & verdict: OK

---
+++ 500-599/522/(29800601)[OK]D[ b'Closest Equals' ].cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+
+int parr[500005 + 1];
+int A[500005 + 1];
+unordered_map<int, int> last;
+unordered_map<int, vector<pii>> intervals;
+
+void ad(int ind, int val)
+{
+    while(ind>0)
+    {
+        parr[ind] = min(parr[ind], val);
+        ind = ind - (ind & -ind);
+    }
+}
+
+int mi(int ind)
+{
+    int ans = 1e9;
+    while(ind<=500005)
+    {
+        ans = min(ans, parr[ind]);
+        ind = ind + (ind & -ind);
+    }
+    return ans;
+}
+
+int ans[500005 + 1];
+
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    memset(parr, 127, sizeof(parr));
+    last.reserve(1000000);
+    intervals.reserve(1000000);
+    int n, m, l, r;
+    cin>>n>>m;
+    for(int i = 1; i <= n; ++i)
+        cin>>A[i];
+    for(int i = 0; i < m; ++i)
+    {
+        cin>>l>>r;
+        intervals[r].push_back(pii(l,i));
+    }
+    for(int i = 1; i <= n; ++i)
+    {
+        if(last.count(A[i]))
+            ad(last[A[i]], i - last[A[i]]);
+        last[A[i]] = i;
+        for(auto a: intervals[i])
+        {
+            ans[a.second] = mi(a.first);
+            if(ans[a.second]>n)
+                ans[a.second] = -1;
+        }
+    }
+    for(int i = 0; i < m; ++i)
+        cout<<ans[i]<<'\n';
+    return 0;
+}
