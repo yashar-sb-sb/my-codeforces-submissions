59792f380480e5faccd596803ba26067e8e2ce6b
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-08-28 00:00:03 +0430

    submit 29833349 for 808/D/[Array Division] with lang: GNU C++14 & verdict: WRONG_ANSWER

---
+++ 800-899/808/(29833349)[WRONG_ANSWER]D[ b'Array Division' ].cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int A[100000];
+set<int> se;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int n;
+    cin>>n;
+    LL sum = 0;
+    for(int i = 0; i < n; ++i)
+    {
+        cin>>A[i];
+        sum += A[i];
+    }
+
+    if(sum&1)
+        return cout<<"NO"<<endl, 0;
+
+    LL suml = 0;
+    for(int i = 0; i < n; ++i)
+    {
+        se.insert(A[i]);
+        suml += A[i];
+        se.insert(A[i]);
+        if(se.count((suml*2 - sum)/2))
+            return cout<<"YES"<<endl, 0;
+    }
+
+    reverse(A, A+n);
+
+    suml = 0;
+    se.clear();
+    for(int i = 0; i < n; ++i)
+    {
+        se.insert(A[i]);
+        suml += A[i];
+        se.insert(A[i]);
+        if(se.count((suml*2 - sum)/2))
+            return cout<<"YES"<<endl, 0;
+    }
+
+    cout<<"NO"<<endl;
+
+    return 0;
+}
