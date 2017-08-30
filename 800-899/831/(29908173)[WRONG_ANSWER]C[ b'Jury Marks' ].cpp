3557b82c42c322a35ce852cc05f93b1824f75a5c
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-08-30 09:33:36 +0430

    submit 29908173 for 831/C/[Jury Marks] with lang: GNU C++14 & verdict: WRONG_ANSWER

---
+++ 800-899/831/(29908173)[WRONG_ANSWER]C[ b'Jury Marks' ].cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+
+int A[2000], B[2000];
+
+
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int k,n;
+    cin>>k>>n;
+    for(int i = 0; i < k; ++i)
+        cin>>A[i];
+    for(int i = 1; i < k; ++i)
+        A[i] += A[i-1];
+    sort(A, A+k);
+    for(int i = 0; i < n; ++i)
+        cin>>B[i];
+    sort(B, B+k);
+    int ans = 0;
+    for(int i = 0; i <= k-n; ++i)
+    {
+        int diff = A[i] - B[i];
+        bool f = true;
+        int l = i;
+        for(int j = 0; j < n; ++j)
+        {
+            while(l<k && A[l] - B[j] != diff)
+                ++l;
+            if(l == k)
+            {
+                f = false;
+                break;
+            }
+        }
+        ans += f && diff<=0;
+    }
+    cout<<ans<<endl;
+    return 0;
+}
