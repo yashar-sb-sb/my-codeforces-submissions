5f38c08a9b58def2d27fc5532496b51c33baec14
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-09-03 11:29:01 +0430

    submit 30025899 for 792/C/[Divide by Three] with lang: GNU C++14 & verdict: OK

---
+++ 700-799/792/(30025899)[OK]C[ b'Divide by Three' ].cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+string f(int c, int m, string s)
+{
+    for(int i = s.size()-1; i >= 0 && c; --i)
+        if((s[i]-'0')%3 == m)
+            --c, s[i] = '+';
+    string ans = "";
+    if(c)
+        return ans;
+    int ind = 0;
+    while(ind < int(s.size()) && (s[ind] == '+' || s[ind] == '0'))
+        ++ind;
+    while(ind < int(s.size()))
+    {
+        if(s[ind]!='+')
+            ans.push_back(s[ind]);
+        ++ind;
+    }
+    if(!ans.size() && s.back() == '0')
+        return "0";
+    return ans;
+}
+
+
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    string s;
+    cin>>s;
+    int z = -1;
+    int m = 0;
+    for(char c:s)
+        if(c=='0')
+            z = 0;
+    for(char c:s)
+        m += c-'0';
+    m %= 3;
+    if(!m)
+        return cout<<s<<endl, 0;
+    string t = f(1, m, s);
+    string d = f(2, m*2%3, s);
+    if(t.size() > d.size())
+        s = t;
+    else
+        s = d;
+    if(s.size())
+        cout<<s<<endl;
+    else
+        cout<<z<<endl;
+    return 0;
+}
