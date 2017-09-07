e4ad131ed06f2ee2f5e2d2d1c5c86163dd184b98
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-09-07 17:28:07 +0430

    submit 30181730 for 817/C/[Really Big Numbers] with lang: GNU C++14 & verdict: WRONG_ANSWER

---
+++ 800-899/817/(30181730)[WRONG_ANSWER]C[ b'Really Big Numbers' ].cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+LL n, s;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    cin>>n>>s;
+    LL ans = n-s+1;
+    for(LL i = s; i <= n && i-s <= 1000000; ++i)
+    {
+        LL t = i;
+        LL sum = 0;
+        while(t)
+        {
+            sum += t%10;
+            t /= 10;
+        }
+        if(i-sum < s)
+            --ans;
+    }
+    cout<<ans<<endl;
+    return 0;
+}
