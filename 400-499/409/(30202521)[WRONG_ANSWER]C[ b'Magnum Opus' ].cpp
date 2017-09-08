09e768a242aded4a586358c7738694223f242a07
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-09-08 17:14:15 +0430

    submit 30202521 for 409/C/[Magnum Opus] with lang: GNU C++14 & verdict: WRONG_ANSWER

---
+++ 400-499/409/(30202521)[WRONG_ANSWER]C[ b'Magnum Opus' ].cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    cout<<1<<endl;
+    return 0;
+}
