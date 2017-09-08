9da3aa846ceaf479e3b6be991b7d8c60215c2cec
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-09-08 17:11:11 +0430

    submit 30202454 for 409/H/[A + B Strikes Back] with lang: GNU C++14 & verdict: WRONG_ANSWER

---
+++ 400-499/409/(30202454)[WRONG_ANSWER]H[ b'A + B Strikes Back' ].cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int a, b;
+    cin>>a>>b;
+    cout<<a+b<<endl;
+    return 0;
+}
