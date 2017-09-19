7eb3739bf96133f35f56e1977eb4eff459751373
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-09-19 20:17:49 +0430

    submit 30511408 for 862/C/[Mahmoud and Ehab and the xor] with lang: GNU C++14 & verdict: OK

---
+++ 800-899/862/(30511408)[OK]C[ b'Mahmoud and Ehab and the xor' ].cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int n,x;
+    cin>>n>>x;
+    if(!x && n == 2)
+        return cout<<"NO"<<endl, 0;
+    cout<<"YES"<<endl;
+    if(n == 1)
+    {
+        cout<<x<<endl;
+    }
+    else if(n == 2)
+    {
+        cout<<0<<' '<<x<<endl;
+    }
+    else
+    {
+        cout<<0b1110100001001000000<<' '<<0b10110100001001000000<<' ';
+        x ^= 0b1110100001001000000;
+        x ^= 0b10110100001001000000;
+        for(int i = 0; i < n-3; ++i)
+        {
+            x^=i;
+            cout<<i<<' ';
+        }
+        cout<<x<<endl;
+    }
+    return 0;
+}
