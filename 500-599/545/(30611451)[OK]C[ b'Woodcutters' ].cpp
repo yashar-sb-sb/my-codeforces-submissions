8ad80a2965e24f8927445a9ddb1c5a9f280ef463
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-09-22 13:53:04 +0330

    submit 30611451 for 545/C/[Woodcutters] with lang: GNU C++14 & verdict: OK

---
+++ 500-599/545/(30611451)[OK]C[ b'Woodcutters' ].cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int n;
+    cin>>n;
+    pii cu, ne;
+    int ans = 1, p = -1e9;
+    cin>>cu.first>>cu.second;
+    for(int i = 1; i < n; ++i)
+    {
+        cin>>ne.first>>ne.second;
+        if(cu.first-cu.second>p)
+        {
+            p = cu.first;
+            ++ans;
+        }
+        else if(cu.first+cu.second<ne.first)
+        {
+            ++ans;
+            p = cu.first+cu.second;
+        }
+        else
+            p = cu.first;
+        cu = ne;
+    }
+    cout<<ans<<endl;
+    return 0;
+}
