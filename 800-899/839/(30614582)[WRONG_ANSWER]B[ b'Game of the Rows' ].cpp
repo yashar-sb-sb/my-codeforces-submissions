ab77352cc266c7c9ffdfbcfe10a8241ea39cf7e9
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-09-22 16:13:19 +0330

    submit 30614582 for 839/B/[Game of the Rows] with lang: GNU C++14 & verdict: WRONG_ANSWER

---
+++ 800-899/839/(30614582)[WRONG_ANSWER]B[ b'Game of the Rows' ].cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int n, k;
+int A[5];
+
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    cin>>n>>k;
+    A[2] = 2*n;
+    A[4] = n;
+    int inp;
+    for(int i = 0; i < k; ++i)
+    {
+        cin>>inp;
+        int s;
+        for(int j = 4; j > 0; --j)
+        {
+            s = min(A[j], inp/j);
+            inp -= s*j;
+            A[j] -= s;
+        }
+        for(int j = 4; j > 0 && inp; --j)
+        {
+            if(A[j])
+            {
+                ++A[j-inp-1];
+                --A[j];
+                inp = 0;
+            }
+        }
+        if(inp)
+            return cout<<"NO"<<endl, 0;
+    }
+    cout<<"YES"<<endl;
+    return 0;
+}
