a571e63ec1576e917eadf1f20c1eb5163316f246
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-09-22 16:46:04 +0330

    submit 30615415 for 839/B/[Game of the Rows] with lang: GNU C++14 & verdict: WRONG_ANSWER

---
+++ 800-899/839/(30615415)[WRONG_ANSWER]B[ b'Game of the Rows' ].cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int n, k;
+vector<int> v;
+int A[5];
+
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    cin>>n>>k;
+    A[2] = 2*n;
+    A[4] = n;
+    for(int i = 0; i < k; ++i)
+    {
+        int inp;
+        cin>>inp;
+        v.push_back(inp);
+    }
+    for(int &inp :v)
+    {
+        int s;
+        for(int j = 4; j > 0; --j)
+        {
+            s = min(A[j], inp/j);
+            inp -= s*j;
+            A[j] -= s;
+        }
+    }
+    for(int &inp :v)
+    {
+        if(inp == 3 && A[1] && A[2])
+        {
+            inp = 0;
+            --A[1];
+            --A[2];
+        }
+        if(inp == 2 && A[1] > 1)
+        {
+            inp = 0;
+            A[1] -= 2;
+        }
+        for(int j = 4; j > 0 && inp; --j)
+        {
+            while(A[j] && inp)
+            {
+                if(j > inp)
+                {
+                    ++A[j-inp-1];
+                    --A[j];
+                    inp = 0;
+                }
+                else
+                {
+                    --A[j];
+                    inp -= j;
+                }
+            }
+        }
+        if(inp)
+            return cout<<"NO"<<endl, 0;
+    }
+    cout<<"YES"<<endl;
+    return 0;
+}
+    int inp;
