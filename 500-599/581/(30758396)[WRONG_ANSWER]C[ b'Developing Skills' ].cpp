e14fc949f902c57b078c80689c1ae18255cdf62a
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-09-26 20:50:44 +0330

    submit 30758396 for 581/C/[Developing Skills] with lang: GNU C++14 & verdict: WRONG_ANSWER

---
+++ 500-599/581/(30758396)[WRONG_ANSWER]C[ b'Developing Skills' ].cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int A[100000];
+
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int n, k;
+    cin>>n>>k;
+    for(int i = 0; i < n; ++i)
+        cin>>A[i];
+    for(int r = 9; r >= 0; --r)
+    {
+        for(int i = 0; i < n; ++i)
+            if(A[i]%10 == r && A[i] != 100)
+            {
+                A[i] += min(10-r, k);
+                k -= min(10-r, k);
+            }
+    }
+    int ans = 0;
+    for(int i = 0; i < n; ++i)
+        ans += A[i] / 10;
+    cout<<ans<<endl;
+    return 0;
+}
