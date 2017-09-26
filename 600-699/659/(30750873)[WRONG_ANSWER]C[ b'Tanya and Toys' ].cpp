dc717933de684d6532e071b062b97f55dda7fcad
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-09-26 16:15:54 +0330

    submit 30750873 for 659/C/[Tanya and Toys] with lang: GNU C++14 & verdict: WRONG_ANSWER

---
+++ 600-699/659/(30750873)[WRONG_ANSWER]C[ b'Tanya and Toys' ].cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+unordered_set<int> se;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int n, m, inp;
+    cin>>n>>m;
+    for(int i = 0; i < n; ++i)
+    {
+        cin>>inp;
+        se.insert(inp);
+    }
+    vector<int> ans;
+    for(int i = 1; i <= m; ++i, m-=i)
+        ans.push_back(i);
+    cout<<ans.size()<<endl;
+    for(auto i:ans)
+        cout<<i<<' ';
+    cout<<endl;
+    return 0;
+}
