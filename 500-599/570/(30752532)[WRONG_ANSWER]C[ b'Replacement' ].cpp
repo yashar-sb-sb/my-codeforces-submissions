b8b068d3876f73f4f91faae05723b0b0584e28b4
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-09-26 17:16:22 +0330

    submit 30752532 for 570/C/[Replacement] with lang: GNU C++14 & verdict: WRONG_ANSWER

---
+++ 500-599/570/(30752532)[WRONG_ANSWER]C[ b'Replacement' ].cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+char S[300002];
+
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int n, m;
+    cin>>n>>m>>(S+1);
+    int count = 0;
+    int sum = 0;
+    for(int i = 0; i <= n+1; ++i)
+    {
+        if(S[i] == '.')
+            ++count;
+        else
+        {
+            sum += max(0, count - 1);
+            count = 0;
+        }
+    }
+    int x;
+    char c;
+    for(int i = 0; i < m; ++i)
+    {
+        cin>>x>>c;
+        if(S[x] == c || S[x] != '.' && c != '.')
+            continue;
+        if(c == '.')
+        {
+            S[x] = '.';
+            sum += (S[x-1] == '.') + (S[x+1] == '.');
+        }
+        else
+        {
+            S[x] = c;
+            sum -= (S[x-1] == '.') + (S[x+1] == '.');
+        }
+        cout<<sum<<'\n';
+    }
+    return 0;
+}
