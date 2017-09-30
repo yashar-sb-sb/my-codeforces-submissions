59e81b1fa0ba6259570663c6e5a23d909fb4e4b6
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-09-30 12:13:12 +0330

    submit 30859610 for 861/B/[Which floor?] with lang: GNU C++14 & verdict: WRONG_ANSWER

---
+++ 800-899/861/(30859610)[WRONG_ANSWER]B[ b'Which floor' ].cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int K[100];
+int F[100];
+
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int n, m;
+    cin>>n>>m;
+    for(int i = 0; i < m; ++i)
+    {
+        cin>>K[i]>>F[i];
+        if(n == K[i])
+            return cout<<F[i]<<endl, 0;
+    }
+    int ans = -1;
+    for(int s = 1; s <= 100; ++s)
+    {
+        for(int i = 0; i < m; ++i)
+            if((K[i]-1)/s!=F[i]-1)
+                goto next;
+        if(ans>0)
+        {
+            ans = -1;
+            break;
+        }
+        ans = (n-1)/s+1;
+        next:;
+    }
+    cout<<ans<<endl;
+    return 0;
+}
