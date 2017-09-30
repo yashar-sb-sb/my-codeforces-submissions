6f9a65e0e598d4f1b48d1e1cdee8eb1cbf211767
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-09-30 20:49:57 +0330

    submit 30875322 for 866/A/[Save the problem!] with lang: GNU C++14 & verdict: OK

---
+++ 800-899/866/(30875322)[OK]A[ b'Save the problem!' ].cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int n;
+    cin>>n;
+    cout<<2*n-1<<' '<<2<<'\n'<<1<<' '<<2<<endl;
+    return 0;
+}
