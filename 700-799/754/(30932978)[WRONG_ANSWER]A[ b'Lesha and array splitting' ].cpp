728d9e6faaac2d8a6caf62e51892fcea20ac7e58
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-10-02 08:11:00 +0330

    submit 30932978 for 754/A/[Lesha and array splitting] with lang: GNU C++14 & verdict: WRONG_ANSWER

---
+++ 700-799/754/(30932978)[WRONG_ANSWER]A[ b'Lesha and array splitting' ].cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int n;
+    cin>>n;
+    int sum = 0;
+    int r = 0;
+    for(int i = 1; i <= n; ++i)
+    {
+        int inp;
+        cin>>inp;
+        sum += inp;
+        if(sum > 0)
+            r = i;
+    }
+    if(sum)
+        return cout<<"YES"<<endl<<1<<endl<<1<<' '<<n<<endl, 0;
+    else if(r)
+        return cout<<"YES"<<endl<<2<<endl<<1<<' '<<r<<endl<<r+1<<' '<<n<<endl, 0;
+    cout<<"NO"<<endl;
+    return 0;
+}
