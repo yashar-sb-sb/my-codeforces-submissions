e02caa01f1ac8840bfa4010caa9d9d1e1602e768
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-10-12 18:37:36 +0330

    submit 31249757 for 873/D/[Merge Sort] with lang: GNU C++14 & verdict: WRONG_ANSWER

---
+++ 800-899/873/(31249757)[WRONG_ANSWER]D[ b'Merge Sort' ].cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int A[100001];
+int n, k;
+
+void f(int l, int r, int b)
+{
+    --k;
+    if(l+1 == r || !k)
+    {
+        while(l < r)
+            A[l++] = b++;
+        return;
+    }
+    int lenl = (l+r)/2 - l;
+    int lenr = r - (l+r)/2;
+    f(l, l+lenl, b+lenr);
+    f(l+lenl, r, b);
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    cin>>n>>k;
+    if(k % 2 == 0)
+        return cout<<-1<<endl, 0;
+    f(0, n, 1);
+    if(k)
+        return cout<<-1<<endl, 0;
+    for(int i = 0; i < n; ++i)
+        cout<<A[i]<<' ';
+    cout<<endl;
+    return 0;
+}
