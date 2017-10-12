6fda775aff6892481c79d4a4e9babdafc6312adb
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-10-12 17:57:51 +0330

    submit 31245949 for 873/B/[Balanced Substring] with lang: GNU C++14 & verdict: WRONG_ANSWER

---
+++ 800-899/873/(31245949)[WRONG_ANSWER]B[ b'Balanced Substring' ].cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int dd[100001], dp[100001];
+
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int n;
+    cin>>n;
+    char c;
+    int bal = 0, ans = 0;
+    for(int i = 1; i <= n; ++i)
+    {
+        cin>>c;
+        bal += c == '0' ? -1 : 1;
+        if(!dp[bal] && bal)
+            dp[bal] = i;
+        else
+            ans = max(i - dp[bal], ans);
+    }
+    cout<<ans<<endl;
+    return 0;
+}
