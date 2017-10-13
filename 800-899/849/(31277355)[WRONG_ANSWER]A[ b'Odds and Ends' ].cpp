67685fd68f58c954a1e9a91f1748ae09d4506e0f
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-10-13 18:04:09 +0330

    submit 31277355 for 849/A/[Odds and Ends] with lang: GNU C++14 & verdict: WRONG_ANSWER

---
+++ 800-899/849/(31277355)[WRONG_ANSWER]A[ b'Odds and Ends' ].cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int A[101];
+
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int n;
+    cin>>n;
+    for(int i = 0; i < n; ++i)
+        cin>>A[i];
+    if(n % 2 == 0 || A[0] % 2 == 0 || A[n-1] % 2 == 0)
+        return cout<<"No"<<endl, 0;
+    int l, r;
+    l = 0, r = 0;
+    while(r < n && (A[r] % 2 == 0 || A[r+1] % 2 == 0))
+        r += 2;
+    if(r >= n)
+        return cout<<"No"<<endl, 0;
+    l = r = r + 1;
+    while(r < n && (A[r] % 2 == 0 || A[r+1] % 2 == 0))
+        r += 2;
+    if(r >= n-1)
+        return cout<<"No"<<endl, 0;
+    cout<<"Yes"<<endl;
+    return 0;
+}
