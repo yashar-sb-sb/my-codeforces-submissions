f7e981c0259d8dafeecffcf479d4071fa3be8ebf
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-10-13 11:45:34 +0330

    submit 31268459 for 844/D/[Interactive LowerBound] with lang: GNU C++14 & verdict: WRONG_ANSWER

---
+++ 800-899/844/(31268459)[WRONG_ANSWER]D[ b'Interactive LowerBound' ].cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int n, start, x;
+int best = -1;
+int all = 1999;
+bool bag[50001];
+void go()
+{
+    int ind = 0;
+    for(int i = 0; i < 10 && bag[ind]; ++i)
+        ind = rand()%n + 1;
+    int count = n+1;
+    while(bag[ind] && count--)
+        ind = ind%n+1;
+    if(count<0)
+        return;
+    bag[ind] = true;
+    --all;
+    cout<<"? "<<ind<<endl;
+    int val, nex;
+    cin>>val>>nex;
+    if(val <= x && val > best)
+    {
+        start = ind;
+        best = val;
+    }
+    else if(nex != -1)
+        bag[nex] = true;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    srand(1e9+7);
+    bag[0] = true;
+    cin>>n>>start>>x;
+    bag[start]= true;
+    for(int i = 0; i < 1000; ++i)
+        go();
+    while(best < x && start != -1 && --all)
+    {
+        cout<<"? "<<start<<endl;
+        int val, nex;
+        cin>>val>>nex;
+        start = nex;
+        best = val;
+    }
+    cout<<"! ";
+    if(best < x)
+        cout<<-1<<endl;
+    else
+        cout<<best<<endl;
+    return 0;
+}
