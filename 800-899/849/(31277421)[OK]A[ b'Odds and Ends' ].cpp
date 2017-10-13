3cb4f1b35f8c0873f36e81e80e29472ef6ad62b7
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-10-13 18:06:45 +0330

    submit 31277421 for 849/A/[Odds and Ends] with lang: GNU C++14 & verdict: OK

---
+++ 800-899/849/(31277421)[OK]A[ b'Odds and Ends' ].cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int n;
+    cin>>n;
+    if(n % 2 == 0)
+        return cout<<"No"<<endl, 0;
+    cin>>n;
+    if(n % 2 == 0)
+        return cout<<"No"<<endl, 0;
+    while(cin>>n);
+    if(n % 2 == 0)
+        return cout<<"No"<<endl, 0;
+    cout<<"Yes"<<endl;
+    return 0;
+}
