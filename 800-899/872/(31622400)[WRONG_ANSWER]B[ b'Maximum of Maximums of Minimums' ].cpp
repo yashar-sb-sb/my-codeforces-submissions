a9f135f3219cc255c538edc50cfcea1f2c3024d6
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-10-23 11:34:51 +0330

    submit 31622400 for 872/B/[Maximum of Maximums of Minimums] with lang: GNU C++14 & verdict: WRONG_ANSWER

---
+++ 800-899/872/(31622400)[WRONG_ANSWER]B[ b'Maximum of Maximums of Minimums' ].cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int n, k, inp;
+    cin>>n>>k;
+    if(k == 1)
+    {
+        int mi = 1e9;
+        for(int i = 0; i < n; ++i)
+        {
+            cin>>inp;
+            mi = min(mi, inp);
+        }
+        cout<<mi<<endl;
+    }
+    else
+    {
+        int ma = -1e9;
+        for(int i = 0; i < n; ++i)
+        {
+            cin>>inp;
+            ma = max(ma, inp);
+        }
+        cout<<ma<<endl;
+    }
+    return 0;
+}
