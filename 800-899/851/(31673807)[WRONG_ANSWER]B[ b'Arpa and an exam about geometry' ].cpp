91ca7091610d65289e2868945ebc441eb893b647
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-10-24 12:00:07 +0330

    submit 31673807 for 851/B/[Arpa and an exam about geometry] with lang: GNU C++14 & verdict: WRONG_ANSWER

---
+++ 800-899/851/(31673807)[WRONG_ANSWER]B[ b'Arpa and an exam about geometry' ].cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+LL dist(LL x, LL y, LL X, LL Y)
+{
+    X -= x;
+    Y -= y;
+    return X*X + Y*Y;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int ax, ay, bx, by, cx, cy;
+    cin>> ax>> ay>> bx>> by>> cx>> cy;
+    int X1 = bx-ax, Y1 = by-ay, X2 = cx-bx, Y2 = cy-bx;
+    cout<<(dist(ax, ay, bx, by) == dist(bx, by, cx, cy) && X1*Y2 != X2*Y1 ? "Yes" : "No")<<endl;
+    return 0;
+}
