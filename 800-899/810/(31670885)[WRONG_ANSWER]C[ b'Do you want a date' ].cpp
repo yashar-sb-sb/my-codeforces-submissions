4fc11e97a14de2f62d8e1849a3f2fc9da6879185
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-10-24 10:02:45 +0330

    submit 31670885 for 810/C/[Do you want a date?] with lang: GNU C++14 & verdict: WRONG_ANSWER

---
+++ 800-899/810/(31670885)[WRONG_ANSWER]C[ b'Do you want a date' ].cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int A[int(3e5)];
+int mod = 1e9 + 7;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int n;
+    cin>>n;
+    for(int i = 0; i < n; ++i)
+        cin>>A[i];
+    sort(A, A+n);
+    LL ans = 0;
+    LL two = 2;
+    for(int i = 1; i < n; ++i)
+    {
+        ans = 3LL*ans % mod;
+        ans = (ans + (two+mod-1)%mod * LL(A[i] - A[i-1])) % mod;
+        two = two * 2LL % mod;
+    }
+    cout<<ans<<endl;
+    return 0;
+}
