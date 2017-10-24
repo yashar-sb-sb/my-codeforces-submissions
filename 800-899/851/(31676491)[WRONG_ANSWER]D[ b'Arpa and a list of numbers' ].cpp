920b3a945bfab2c0358b286956dc04cbbd674a11
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-10-24 13:30:18 +0330

    submit 31676491 for 851/D/[Arpa and a list of numbers] with lang: GNU C++14 & verdict: WRONG_ANSWER

---
+++ 800-899/851/(31676491)[WRONG_ANSWER]D[ b'Arpa and a list of numbers' ].cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+vector<int> primes;
+int A[int(5e5)];
+
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int n, x, y;
+    cin>>n>>x>>y;
+    for(int i = 2; i < 1000; ++i)
+    {
+        bool prime = true;
+        for(auto p: primes)
+            if(i%p == 0)
+            {
+                prime = false;
+                break;
+            }
+        if(prime)
+            primes.push_back(i);
+    }
+    for(int i = 0; i < n; ++i)
+        cin>>A[i];
+    LL ans = x*LL(n);
+    for(int p:primes)
+    {
+        LL cur = 0;
+        for(int i = 0; i < n; ++i)
+            cur += min(LL(y)*((p-A[i]%p)%p), LL(x));
+        ans = min(ans, cur);
+    }
+    cout<<ans<<endl;
+    return 0;
+}
