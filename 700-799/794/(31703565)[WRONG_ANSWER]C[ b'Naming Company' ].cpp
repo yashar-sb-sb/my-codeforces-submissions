df62176deabbdf61bf40d7ee98377c1c4282792b
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-10-25 12:32:23 +0330

    submit 31703565 for 794/C/[Naming Company] with lang: GNU C++14 & verdict: WRONG_ANSWER

---
+++ 700-799/794/(31703565)[WRONG_ANSWER]C[ b'Naming Company' ].cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int n;
+string f, s;
+char ans[300001];
+
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    cin>>f>>s;
+    n = f.size();
+    sort(f.begin(), f.end());
+    sort(s.begin(), s.end(), greater<int>());
+    auto fi = f.begin();
+    auto si = s.begin();
+    int l = 0, r = n-1;
+    for(int i = 0; i < n; ++i)
+    {
+        if(!(i&1))
+        {
+            if(*fi < *si)
+                ans[l++] = *fi++;
+            else
+                ans[r--] = *fi++;
+        }
+        else
+        {
+            if(*si > *fi)
+                ans[l++] = *si++;
+            else
+                ans[r--] = *si++;
+        }
+    }
+    cout<<ans<<endl;
+    return 0;
+}
