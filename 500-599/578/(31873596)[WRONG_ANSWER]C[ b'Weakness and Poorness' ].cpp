278d0c7a0eb57223e4bcf60979c3f264800d08be
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-10-29 21:07:46 +0330

    submit 31873596 for 578/C/[Weakness and Poorness] with lang: GNU C++14 & verdict: WRONG_ANSWER

---
+++ 500-599/578/(31873596)[WRONG_ANSWER]C[ b'Weakness and Poorness' ].cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int n;
+int A[int(2e5)];
+
+ldb f(ldb x)
+{
+    ldb mi = 0, ma = 0, cur = 0, ans = 0;
+    for(int i = 0; i < n; ++i)
+    {
+        cur += A[i] -x;
+        mi = min(mi, cur);
+        ma = max(ma, cur);
+        ans = max(ans, max(abs(cur-ma), abs(cur-mi)));
+    }
+    return ans;
+}
+
+
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    cin>>n;
+    for(int i = 0; i < n; ++i)
+        cin>>A[i];
+    ldb l = -10000000, r = 10000000;
+    ldb m1, m2;
+    while(r-l > 1e-10)
+    {
+        m1 = (l+l+r)/3;
+        m2 = (l+r+r)/3;
+        if(f(m1) < f(m2))
+            r = m2;
+        else
+            l = m1;
+    }
+    cout.precision(12);
+    cout.setf(ios::fixed);
+    cout<<f((l+r)/2)<<endl;
+    return 0;
+}
