da151fb97ba61ba8ae31361f11dfdf01b336d297
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-10-29 10:54:22 +0330

    submit 31858837 for 787/B/[Not Afraid] with lang: GNU C++14 & verdict: WRONG_ANSWER

---
+++ 700-799/787/(31858837)[WRONG_ANSWER]B[ b'Not Afraid' ].cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int n, m;
+    cin>>n>>m;
+    for(int i = 0; i < m; ++i)
+    {
+        set<int> se;
+        int k;
+        cin>>k;
+        bool end = true;
+        while(k--)
+        {
+            int a;
+            cin>>a;
+            if(se.count(-a))
+            {
+                end = false;
+                break;
+            }
+            se.insert(a);
+        }
+        if(end)
+            return cout<<"YES"<<endl, 0;
+    }
+    cout<<"NO"<<endl;
+    return 0;
+}
