d38193e18fba6ebdfb943feba297ff58411f6b24
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-10-31 19:15:19 +0330

    submit 31926337 for 813/B/[The Golden Age] with lang: GNU C++14 & verdict: WRONG_ANSWER

---
+++ 800-899/813/(31926337)[WRONG_ANSWER]B[ b'The Golden Age' ].cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int x, y, l, r;
+    cin>>x>>y>>l>>r;
+    LL A = 1, B = 1;
+    vector<LL> ve = {l-1, r+1};
+    while(A<r)
+    {
+        B = 1;
+        while(A+B <= r)
+        {
+            if(A+B >= l)
+                ve.push_back(A+B);
+            B*=y;
+        }
+        A *= x;
+    }
+    sort(ve.begin(), ve.end());
+    LL ans = 0;
+    for(int i = 1; i < int(ve.size()); ++i)
+        ans = max(ans, ve[i] - ve[i-1]-1);
+    cout<<ans<<endl;
+    return 0;
+}
