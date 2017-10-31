f1021029cdd805aa6d0f435eed226359217299dc
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-10-31 18:54:45 +0330

    submit 31925827 for 813/A/[The Contest] with lang: GNU C++14 & verdict: OK

---
+++ 800-899/813/(31925827)[OK]A[ b'The Contest' ].cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int n, m;
+    cin>>n;
+    int sum = 0;
+    int inp;
+    for(int i = 0; i < n; ++i)
+    {
+        cin>>inp;
+        sum += inp;
+    }
+    cin>>m;
+    for(int i = 0; i < m; ++i)
+    {
+        int l, r;
+        cin>>l>>r;
+        if(sum <= r)
+            return cout<<max(l, sum)<<endl, 0;
+    }
+    cout<<"-1\n";
+    return 0;
+}
