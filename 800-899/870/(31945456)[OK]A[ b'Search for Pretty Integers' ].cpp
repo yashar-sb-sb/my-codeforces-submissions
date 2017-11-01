4492726d3d4f950d532ac5e05d35385fc5f3899c
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-11-01 13:41:03 +0330

    submit 31945456 for 870/A/[Search for Pretty Integers] with lang: GNU C++14 & verdict: OK

---
+++ 800-899/870/(31945456)[OK]A[ b'Search for Pretty Integers' ].cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int A[10];
+int B[10];
+
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int n, m, inp;
+    cin>>n>>m;
+    for(int i = 0; i < n; ++i)
+        ++A[cin>>inp, inp];
+    for(int i = 0; i < m; ++i)
+        ++B[cin>>inp, inp];
+    for(int i = 0; i < 10; ++i)
+        if(A[i] && B[i])
+            return cout<<i<<endl, 0;
+    int a, b;
+    for(int i = 0; i < 10; ++i)
+        if(A[i])
+        {
+            a = i;
+            break;
+        }
+    for(int i = 0; i < 10; ++i)
+        if(B[i])
+        {
+            b = i;
+            break;
+        }
+    cout<<min(a, b)<<max(a, b)<<endl;
+    return 0;
+}
