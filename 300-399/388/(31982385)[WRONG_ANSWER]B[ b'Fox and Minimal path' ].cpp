236befd2eab8656900a630f96968b165f76c520c
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-11-02 17:28:24 +0330

    submit 31982385 for 388/B/[Fox and Minimal path] with lang: GNU C++14 & verdict: WRONG_ANSWER

---
+++ 300-399/388/(31982385)[WRONG_ANSWER]B[ b'Fox and Minimal path' ].cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+char A[1000][1001];
+#define S(i,j) A[i][j]=A[j][i]='Y'
+
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int k;
+    cin>>k;
+    int ml = k;
+    memset(A, 'N', sizeof(A));
+    int start = 2;
+    while(k)
+    {
+        int left = start++;
+        int mid = start++;
+        int right = start++;
+        int cur = 1;
+        S(left,0);
+        S(right,0);
+        S(mid,0);
+        int l = 1;
+        while(cur*3LL<=k)
+        {
+            l*=3;
+            cur *= 3;
+            int nl = start++;
+            int nr = start++;
+            int nm = start++;
+            S(left, nr);
+            S(left, nl);
+            S(left, nm);
+
+            S(right, nr);
+            S(right, nm);
+            S(right, nl);
+
+            S(mid, nr);
+            S(mid, nm);
+            S(mid, nl);
+
+            left = nl;
+            right = nr;
+            mid = nm;
+        }
+        while(l*3LL<=ml)
+        {
+            l*=3;
+            int nl = start++;
+            S(left, nl);
+            left = nl;
+        }
+        k-=cur;
+        S(left,1);
+    }
+    cout<<start<<'\n';
+    for(int i = 0; i < start; ++i)
+    {
+        for(int j = 0; j < start; ++j)
+            cout<<A[i][j];
+        cout<<'\n';
+    }
+    return 0;
+}
