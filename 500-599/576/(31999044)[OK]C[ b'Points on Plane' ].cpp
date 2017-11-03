dd84cec036dc829b11f47cba4ad195e8f789855d
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-11-03 10:34:26 +0330

    submit 31999044 for 576/C/[Points on Plane] with lang: GNU C++14 & verdict: OK

---
+++ 500-599/576/(31999044)[OK]C[ b'Points on Plane' ].cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+vector<pii> A[1001]; 
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int n;
+    cin>>n;
+    for(int i = 1; i <= n; ++i)
+    {
+        int x, y;
+        cin>>x>>y;
+        A[x/1000].emplace_back(y, i);
+    }
+    for(int i = 0; i < 1001; ++i)
+    {
+        if(i&1)sort(A[i].begin(), A[i].end());
+        else sort(A[i].begin(), A[i].end(), greater<pii>());
+        for(pii j: A[i])
+            cout<<j.second<<' ';
+    }
+    cout<<endl;
+    return 0;
+}
