dd4d5b7c7c8656402735749804ce058d61d1e9e7
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-11-07 19:23:53 +0330

    submit 32124457 for 848/A/[From Y to Y] with lang: GNU C++14 & verdict: WRONG_ANSWER

---
+++ 800-899/848/(32124457)[WRONG_ANSWER]A[ b'From Y to Y' ].cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int A[1000];
+
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    for(int i = 2; i < 1000; ++i)
+        A[i] = i-1+A[i-1];
+    int k;
+    cin>>k;
+    int cur = 999;
+    for(char i = 'a'; i <= 'z'; ++i)
+    {
+        while(A[cur]>k)
+            --cur;
+        k -= A[cur];
+        for(int j = 0; j < A[cur]; ++j)
+            cout<<i;
+        if(!k)
+            break;
+    }
+    cout<<endl;
+    return 0;
+}
