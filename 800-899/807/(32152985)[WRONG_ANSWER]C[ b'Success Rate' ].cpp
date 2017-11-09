e3ff0af2cde509e940a0bb52f505e23c50fc8943
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-11-09 11:32:38 +0330

    submit 32152985 for 807/C/[Success Rate] with lang: GNU C++14 & verdict: WRONG_ANSWER

---
+++ 800-899/807/(32152985)[WRONG_ANSWER]C[ b'Success Rate' ].cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    LL x, y, p, q;
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        cin>>x>>y>>p>>q;
+        if(x!=y && p==q)
+        {
+            cout<<-1<<'\n';
+            continue;
+        }
+        if(p==0)
+        {
+            if(x==0)
+                cout<<0<<endl;
+            else
+                cout<<-1<<endl;
+            return 0;
+        }
+        LL a = 0;
+        if(p)
+            a = (x-1)/p;
+        LL b = (y-1)/q;
+        LL P=p+max(a,b)*p;
+        LL Q=q+max(a,b)*q;
+        if(Q-P<y-x)
+        {
+            a = ((y-x)-(Q-P)-1)/(q-p)+1;
+            P+=a*p;
+            Q+=a*q;
+        }
+        cout<<Q-y<<'\n';
+    }
+    return 0;
+}
