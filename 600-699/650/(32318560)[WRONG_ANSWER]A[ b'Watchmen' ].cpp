dbe1875929880a26b18971f43ce9adb2dc98b897
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-11-14 22:42:43 +0330

    submit 32318560 for 650/A/[Watchmen] with lang: GNU C++14 & verdict: WRONG_ANSWER

---
+++ 600-699/650/(32318560)[WRONG_ANSWER]A[ b'Watchmen' ].cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef long double ldb;
+typedef pair<int,int> pii;
+
+unordered_map<int, int> I, J;
+unordered_map<LL, int> A;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int n;
+    cin>>n;
+    while(n--)
+    {
+        int i, j;
+        cin>>i>>j;
+        ++I[i];
+        ++J[j];
+        ++A[(LL(i)<<32) | j];
+    }
+    LL ans = 0;
+    for(pii i:I)
+        ans += (LL)i.second*(i.second-1)/2;
+    for(pii i:J)
+        ans += (LL)i.second*(i.second-1)/2;
+    for(auto i:A)
+        ans -= (LL)i.second*(i.second-1)/2;
+    cout<<ans<<endl;
+    return 0;
+}
